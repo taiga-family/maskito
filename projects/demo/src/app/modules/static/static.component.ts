9eb64cbb9b1d24375c2ed43c6ebb51ebbcfed67d
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-11-07 15:02:27 +0300

    chore: configure dummy sandbox

--- projects/demo/src/app/modules/static/static.component.ts
+++
@@ -1,8 +0,0 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-
-@Component({
-    selector: 'static',
-    templateUrl: './static.template.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class StaticComponent {}
