082e46dac51e199bd363e6f79d7fd81e41935f87
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-11-28 17:37:38 +0300

    chore: fix comments

--- projects/core/src/lib/utils/index.ts
+++ projects/core/src/lib/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './dom/event-listener';
 export * from './dom/is-before-input-event-supported';
 export * from './dom/is-event-producing-character';
-
 export * from './extend-to-not-empty-range';
