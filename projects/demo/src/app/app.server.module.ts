ab9c0aac0132c893c3eede332c910d102d58655f
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-12-06 14:59:58 +0300

    ci: fail Github-job with Cypress tests on SSR error (#34)
    * ci: fail Github-job with Cypress tests on SSR error
    
    * fix(core): add SSR support
    
    * ci: fix base-href for firebase preview channel

--- projects/demo/src/app/app.server.module.ts
+++ projects/demo/src/app/app.server.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 import {ServerModule} from '@angular/platform-server';
-import {UNIVERSAL_PROVIDERS} from '@ng-web-apis/universal';
+import {UniversalModule} from '@ng-web-apis/universal';
 
 import {AppBrowserModule} from './app.browser.module';
 import {AppComponent} from './app.component';
+import {ServerErrorHandler} from './server-error-handler';
 
 @NgModule({
-    imports: [AppBrowserModule, ServerModule],
+    imports: [AppBrowserModule, ServerModule, UniversalModule],
     bootstrap: [AppComponent],
-    providers: UNIVERSAL_PROVIDERS,
+    providers: [{provide: ErrorHandler, useClass: ServerErrorHandler}],
 })
 export class AppServerModule {}
