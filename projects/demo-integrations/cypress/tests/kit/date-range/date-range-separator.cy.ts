1457f0842abb4ea7d97076e9a7d68e516c5a512c
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-01-27 16:47:18 +0300

    chore(demo-integrations): `DateRange` more cypress tests (#81)

---
+++ projects/demo-integrations/cypress/tests/kit/date-range/date-range-separator.cy.ts
@@ -0,0 +1,53 @@
+import {DemoPath} from '@demo/routes';
+
+describe('DateRange | Separator', () => {
+    describe('/', () => {
+        beforeEach(() => {
+            cy.visit(`/${DemoPath.DateRange}/API?separator=/`);
+            cy.get('#demo-content input')
+                .should('be.visible')
+                .first()
+                .focus()
+                .as('input');
+        });
+
+        it('14/12/1997 – 09/07/2015', () => {
+            cy.get('@input')
+                .type('14121997972015')
+                .should('have.value', '14/12/1997 – 09/07/2015');
+        });
+
+        it('rejects dot as separator', () => {
+            cy.get('@input')
+                .type('1412200011.')
+                .should('have.value', '14/12/2000 – 11')
+                .type('12.')
+                .should('have.value', '14/12/2000 – 11/12');
+        });
+    });
+
+    describe('-', () => {
+        beforeEach(() => {
+            cy.visit(`/${DemoPath.DateRange}/API?separator=-`);
+            cy.get('#demo-content input')
+                .should('be.visible')
+                .first()
+                .focus()
+                .as('input');
+        });
+
+        it('14-12-1997 – 09-07-2015', () => {
+            cy.get('@input')
+                .type('14121997972015')
+                .should('have.value', '14-12-1997 – 09-07-2015');
+        });
+
+        it('rejects dot as separator', () => {
+            cy.get('@input')
+                .type('14')
+                .should('have.value', '14')
+                .type('12.')
+                .should('have.value', '14-12');
+        });
+    });
+});
