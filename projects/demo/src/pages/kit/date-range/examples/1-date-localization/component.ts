263bbf38b8a1780abe6b147f8f4a0ece095ae91f
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-02-06 17:07:53 +0300

    chore(demo): `DateRange` add examples to documentation page

---
+++ projects/demo/src/pages/kit/date-range/examples/1-date-localization/component.ts
@@ -0,0 +1,42 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+import mask from './mask';
+
+@Component({
+    selector: 'date-range-mask-doc-example-1',
+    template: `
+        <tui-input
+            tuiTextfieldCustomContent="tuiIconCalendarLarge"
+            [tuiTextfieldFiller]="filler"
+            [tuiHintContent]="hint"
+            [style.max-width.rem]="30"
+            [(ngModel)]="value"
+        >
+            US format
+            <input
+                tuiTextfield
+                inputmode="decimal"
+                [maskito]="mask"
+            />
+        </tui-input>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DateRangeMaskDocExample1 {
+    private readonly usDateFormatter = new Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+    });
+
+    value = '09/20/2020 – 02/06/2023';
+    readonly filler = 'yyyy/mm/dd – yyyy/mm/dd';
+    readonly mask = mask;
+
+    get hint(): string {
+        return this.value
+            .split(' – ')
+            .map(date => this.usDateFormatter.format(new Date(date)))
+            .join(' – ');
+    }
+}
