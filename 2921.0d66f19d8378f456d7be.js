2de70ec0a64b32798d60849c9d959bf51218aada
nsbarsukov <nikita.s.barsukov@gmail.com>
2023-02-09 12:00:41 +0000

    Deploying to gh-pages from @ Tinkoff/maskito@d5f6a0980003d915a3c0f40acfd64e858d42b351 🚀

---
+++ 2921.0d66f19d8378f456d7be.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmaskito=self.webpackChunkmaskito||[]).push([[2921],{2921:t=>{t.exports="import {maskitoTimeOptionsGenerator} from '@maskito/kit';\n\nexport default maskitoTimeOptionsGenerator({\n    mode: 'HH:MM:SS',\n});\n"}}]);
---
+++ 8729.a53af39e197dfb84ca12.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmaskito=self.webpackChunkmaskito||[]).push([[8729],{8729:e=>{e.exports="import {maskitoTimeOptionsGenerator} from '@maskito/kit';\n\nexport default maskitoTimeOptionsGenerator({\n    mode: 'HH:MM',\n    timeSegmentMaxValues: {hours: 12},\n});\n"}}]);
