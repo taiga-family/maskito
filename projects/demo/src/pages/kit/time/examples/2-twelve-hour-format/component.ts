ece27e85b6e738cb0aee94d863a12263caae80b4
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-02-10 12:41:51 +0300

    chore(demo): `Time` improve example `12-hours format`

--- projects/demo/src/pages/kit/time/examples/2-twelve-hour-format/component.ts
+++ projects/demo/src/pages/kit/time/examples/2-twelve-hour-format/component.ts
@@ -8,19 +8,27 @@ import mask from './mask';
         <tui-input
             tuiTextfieldCustomContent="tuiIconTimeLarge"
             tuiTextfieldFiller="hh:mm"
-            ngModel="11:59"
             [tuiTextfieldLabelOutside]="true"
             [style.max-width.rem]="20"
+            [(ngModel)]="value"
         >
             <input
                 tuiTextfield
                 inputmode="decimal"
                 [maskito]="mask"
+                (blur)="onBlur()"
             />
         </tui-input>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TimeMaskDocExample2 {
+    value = '11:59';
     readonly mask = mask;
+
+    onBlur(): void {
+        const [hours, minutes = ''] = this.value.split(':');
+
+        this.value = [hours, minutes].map(segment => segment.padEnd(2, '0')).join(':');
+    }
 }
