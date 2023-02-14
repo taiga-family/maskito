a47262714fba3dbd1da6c44fbe67cfcac95cf148
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-02-14 16:08:26 +0300

    ci: run prettier after `Changelog`-generation during release

---
+++ scripts/helpers/execute.ts
@@ -0,0 +1,17 @@
+import {CommonExecOptions, execSync} from 'child_process';
+
+import {infoLog} from './colored-log';
+
+export function execute(shell: string, options?: Partial<CommonExecOptions>): string {
+    infoLog(`ᐅ ${shell}`);
+
+    return execSync(
+        shell,
+        options ?? {
+            stdio: `inherit`,
+            encoding: `utf8`,
+        },
+    )
+        ?.toString()
+        .trim();
+}
