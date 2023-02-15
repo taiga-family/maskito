3e1e73f85c02e61f1dfdb965765ac0d8120f7ef3
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-02-15 15:53:48 +0300

    ci: script `getAllVersions` should always return array (#133)

--- scripts/helpers/get-all-versions.ts
+++ scripts/helpers/get-all-versions.ts
@@ -1,7 +1,9 @@
 import {execSync} from 'child_process';
 
 export function getAllVersions(name: string): string[] {
-    return JSON.parse(
+    const versions: string[] | string = JSON.parse(
         execSync(`npm view ${name} versions --json || echo "[]"`).toString(),
     );
+
+    return Array.isArray(versions) ? versions : [versions];
 }
