f9c774aec5ccab1a57dcb178b0c96e9d85def5f1
nsbarsukov <nikita.s.barsukov@gmail.com>
2023-02-16 15:08:10 +0000

    Deploying to gh-pages from @ Tinkoff/maskito@ed64e752d653b4c10dd47ae6928f0e030164c5a2 🚀

---
+++ 1961.0085e5ac2e7e87decb26.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmaskito=self.webpackChunkmaskito||[]).push([[1961],{1961:s=>{s.exports="<input />\n"}}]);
---
+++ 9727.0698d5d7ba01936f5081.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmaskito=self.webpackChunkmaskito||[]).push([[9727],{9727:e=>{e.exports="/* TODO: make better basic customization */\ninput {\n    width: 100%;\n    max-width: 25rem;\n    height: 2rem;\n    box-sizing: border-box;\n}\n"}}]);
