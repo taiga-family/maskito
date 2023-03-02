d043a82561cbed94b19d59d174fa6da7f08d49d4
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-03-02 17:16:10 +0300

    fix(kit): `Number` pads empty integer part when paste from clipboard (#168)

---
+++ projects/kit/src/lib/masks/number/processors/tests/not-empty-integer-part-preprocessor.spec.ts
@@ -0,0 +1,75 @@
+import {createNotEmptyIntegerPartPreprocessor} from '../not-empty-integer-part-preprocessor';
+
+const EMPTY_ELEMENT_STATE = {
+    value: '',
+    selection: [0, 0] as const,
+};
+
+describe('createNotEmptyIntegerPartPreprocessor', () => {
+    describe('precision === 2', () => {
+        const preprocessor = createNotEmptyIntegerPartPreprocessor({
+            decimalSeparator: ',',
+            precision: 2,
+        });
+
+        it('should pad integer part with zero if user inserts "a,"', () => {
+            expect(
+                preprocessor(
+                    {
+                        elementState: EMPTY_ELEMENT_STATE,
+                        data: 'a,',
+                    },
+                    'insert',
+                ),
+            ).toEqual({
+                elementState: EMPTY_ELEMENT_STATE,
+                data: '0a,',
+            });
+        });
+
+        it('should NOT pad integer part with zero if user inserts "aaa1aaa,"', () => {
+            expect(
+                preprocessor(
+                    {
+                        elementState: EMPTY_ELEMENT_STATE,
+                        data: 'aaa1aaa,',
+                    },
+                    'insert',
+                ),
+            ).toEqual({
+                elementState: EMPTY_ELEMENT_STATE,
+                data: 'aaa1aaa,',
+            });
+        });
+
+        it('should pad integer part with zero if user inserts ",3123"', () => {
+            expect(
+                preprocessor(
+                    {
+                        elementState: EMPTY_ELEMENT_STATE,
+                        data: ',3123',
+                    },
+                    'insert',
+                ),
+            ).toEqual({
+                elementState: EMPTY_ELEMENT_STATE,
+                data: '0,3123',
+            });
+        });
+
+        it('should NOT pad integer part with zero if user inserts "aaa0aaa,3123"', () => {
+            expect(
+                preprocessor(
+                    {
+                        elementState: EMPTY_ELEMENT_STATE,
+                        data: 'aaa0aaa,3123',
+                    },
+                    'insert',
+                ),
+            ).toEqual({
+                elementState: EMPTY_ELEMENT_STATE,
+                data: 'aaa0aaa,3123',
+            });
+        });
+    });
+});
