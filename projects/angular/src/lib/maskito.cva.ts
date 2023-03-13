a8118e2f28442afe528996006963c027c347be54
waterplea <alexander@inkin.ru>
2023-03-13 20:29:03 +0800

    chore: remove coverage

--- projects/angular/src/lib/maskito.cva.ts
+++ projects/angular/src/lib/maskito.cva.ts
@@ -1,9 +1,5 @@
-import {Directive, ElementRef, Inject, Input, Optional, Renderer2} from '@angular/core';
-import {
-    COMPOSITION_BUFFER_MODE,
-    DefaultValueAccessor,
-    NG_VALUE_ACCESSOR,
-} from '@angular/forms';
+import {Directive, Input} from '@angular/core';
+import {DefaultValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {MASKITO_DEFAULT_OPTIONS, MaskitoOptions, maskitoTransform} from '@maskito/core';
 
 @Directive({
@@ -25,15 +21,6 @@ import {MASKITO_DEFAULT_OPTIONS, MaskitoOptions, maskitoTransform} from '@maskit
 export class MaskitoCva extends DefaultValueAccessor {
     @Input()
     maskito: MaskitoOptions = MASKITO_DEFAULT_OPTIONS;
-
-    // TODO: Figure out why this is necessary under nx test runner
-    constructor(
-        @Inject(Renderer2) renderer: Renderer2,
-        @Inject(ElementRef) elementRef: ElementRef,
-        @Optional() @Inject(COMPOSITION_BUFFER_MODE) compositionMode: boolean,
-    ) {
-        super(renderer, elementRef, compositionMode);
-    }
 
     override writeValue(value: unknown): void {
         super.writeValue(maskitoTransform(String(value ?? ''), this.maskito));
