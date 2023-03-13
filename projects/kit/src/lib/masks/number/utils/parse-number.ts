824b3aa2a9719337fa3f27602f09fc71963e3c8c
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-03-13 12:01:20 +0300

    fix(kit): `maskitoParseNumber` incorrectly parses negative numbers

--- projects/kit/src/lib/masks/number/utils/parse-number.ts
+++ projects/kit/src/lib/masks/number/utils/parse-number.ts
@@ -1,10 +1,17 @@
+import {CHAR_EM_DASH, CHAR_EN_DASH, CHAR_HYPHEN, CHAR_MINUS} from '../../../constants';
+
 export function maskitoParseNumber(
     maskedNumber: string,
     decimalSeparator: string = '.',
 ): number {
+    const negativeSign = maskedNumber.match(
+        new RegExp(`^[${CHAR_MINUS}\\${CHAR_HYPHEN}${CHAR_EN_DASH}${CHAR_EM_DASH}]`),
+    );
+
     return Number(
-        maskedNumber
-            .replace(new RegExp(`[^\\d\\${decimalSeparator}]`, 'g'), '')
-            .replace(decimalSeparator, '.'),
+        (negativeSign ? CHAR_HYPHEN : '') +
+            maskedNumber
+                .replace(new RegExp(`[^\\d\\${decimalSeparator}]`, 'g'), '')
+                .replace(decimalSeparator, '.'),
     );
 }
