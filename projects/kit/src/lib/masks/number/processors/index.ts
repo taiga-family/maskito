6a9e624ee2db777ef55b3859696f6c1105248a2b
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-03-15 12:45:06 +0300

    fix(kit): `Number` should drop decimal part on paste from clipboard if `precision=0`

--- projects/kit/src/lib/masks/number/processors/index.ts
+++ projects/kit/src/lib/masks/number/processors/index.ts
@@ -5,3 +5,4 @@ export * from './not-empty-integer-part-preprocessor';
 export * from './preudo-character-preprocessor';
 export * from './separator-deletion-preprocessor';
 export * from './thousand-separator-postprocessor';
+export * from './zero-precision-preprocessor';
