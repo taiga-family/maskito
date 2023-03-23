3efbb42f2dd5c4e43ff514da7a82abfc7c4b3a38
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-03-23 16:59:44 +0300

    feat(kit): `DateRange` swaps dates if the 2nd date is less than the 1st one (#212)

---
+++ projects/kit/src/lib/masks/date-range/processors/swap-dates-postprocessor.ts
@@ -0,0 +1,39 @@
+import {MaskitoOptions} from '@maskito/core';
+
+import {
+    isDateStringComplete,
+    parseDateRangeString,
+    parseDateString,
+    segmentsToDate,
+} from '../../../utils';
+
+export function createSwapDatesPostprocessor({
+    dateModeTemplate,
+    datesSeparator,
+}: {
+    dateModeTemplate: string;
+    datesSeparator: string;
+}): NonNullable<MaskitoOptions['postprocessor']> {
+    return ({value, selection}) => {
+        const dateStrings = parseDateRangeString(value, dateModeTemplate, datesSeparator);
+        const isDateRangeComplete =
+            dateStrings.length === 2 &&
+            dateStrings.every(date => isDateStringComplete(date, dateModeTemplate));
+        const [from, to] = selection;
+        const caretAtTheEnd = from >= value.length;
+        const allValueSelected = from === 0 && to >= value.length; // dropping text inside with a pointer
+
+        if (!(caretAtTheEnd || allValueSelected) || !isDateRangeComplete) {
+            return {value, selection};
+        }
+
+        const [fromDate, toDate] = dateStrings.map(dateString =>
+            segmentsToDate(parseDateString(dateString, dateModeTemplate)),
+        );
+
+        return {
+            selection,
+            value: fromDate > toDate ? dateStrings.reverse().join(datesSeparator) : value,
+        };
+    };
+}
