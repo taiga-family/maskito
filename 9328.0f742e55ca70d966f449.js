e472a4d04f830413f5f2ffb108700decdc30ce0f
nsbarsukov <nikita.s.barsukov@gmail.com>
2023-03-31 13:06:41 +0000

    Deploying to gh-pages from @ Tinkoff/maskito@50f0d58ccbfa22d15174d76479a9d642687db099 🚀

---
+++ 9328.0f742e55ca70d966f449.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmaskito=self.webpackChunkmaskito||[]).push([[9328],{5968:o=>{o.exports="import {MaskitoOptions} from '@maskito/core';\nimport {maskitoPrefixPostprocessorGenerator} from '@maskito/kit';\n\nexport default {\n    mask: /^\\$?\\d*$/, // dollar sign or digits\n    postprocessor: maskitoPrefixPostprocessorGenerator('$'),\n} as MaskitoOptions;\n"}}]);
