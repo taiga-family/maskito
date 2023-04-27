38e65bb8da48dfc56f7c842d1342856e1ecce84e
g.lunin <g.lunin@tinkoff.ru>
2023-04-27 19:56:41 +0500

    chore: fix review comments

--- projects/react/src/lib/useMaskito.ts
+++ projects/react/src/lib/useMaskito.ts
@@ -14,15 +14,15 @@ const defaultMaskitoPredicate: MaskitoPredicate = e =>
  * Hook for convenient use of Maskito in React
  * @description For controlled inputs use `onInput` event
  * @param options options used for creating Maskito
- * @param predicate function that can help find nested Input or TextArea
+ * @param elementPredicate function that can help find nested Input or TextArea
  * @returns ref callback to pass it in React Element
  * @example
  * // To avoid unnecessary hook runs with Maskito recreation pass named variables
  * // good example ✅
- * useMaskito({ options: maskitoOptions, predicate: maskitoPredicate })
+ * useMaskito({ options: maskitoOptions, elementPredicate: maskitoPredicate })
  *
  * // bad example ❌
- * useMaskito({ options: { mask: /^.*$/ }, predicate: () => e.querySelector('input') })
+ * useMaskito({ options: { mask: /^.*$/ }, elementPredicate: () => e.querySelector('input') })
  */
 export const useMaskito = ({
     options = MASKITO_DEFAULT_OPTIONS,
