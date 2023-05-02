83b1ce3a1943df9f473cadbbbcfd2201efeb2687
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-05-02 09:45:20 +0300

    chore(demo): improve example for `Phone` recipe (#276)

--- projects/demo/src/pages/recipes/phone/examples/2-kz-phone/mask.ts
+++ projects/demo/src/pages/recipes/phone/examples/2-kz-phone/mask.ts
@@ -24,4 +24,23 @@ export default {
     ],
     // non-removable country prefix
     postprocessor: maskitoPrefixPostprocessorGenerator('+7 '),
+    preprocessor: createTrickyPasteCasesPreprocessor(),
 } as MaskitoOptions;
+
+// Paste "89123456789" => "+7 (912) 345-67-89"
+function createTrickyPasteCasesPreprocessor(): MaskitoOptions['preprocessor'] {
+    const trimPrefix = (value: string): string => value.replace(/^(\+?7?\s?8?)\s?/, '');
+    const countDigits = (value: string): number => value.replace(/\D/g, '').length;
+
+    return ({elementState, data}) => {
+        const {value, selection} = elementState;
+
+        return {
+            elementState: {
+                selection,
+                value: countDigits(value) > 11 ? trimPrefix(value) : value,
+            },
+            data: countDigits(data) >= 11 ? trimPrefix(data) : data,
+        };
+    };
+}
