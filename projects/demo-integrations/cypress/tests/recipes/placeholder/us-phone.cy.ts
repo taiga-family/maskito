c025702abf2f72878139e83f78c413059a792518
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-05-16 16:16:47 +0300

    chore(demo-integrations): add cypress tests for `maskitoWithPlaceholder`

---
+++ projects/demo-integrations/cypress/tests/recipes/placeholder/us-phone.cy.ts
@@ -0,0 +1,62 @@
+import {DemoPath} from '@demo/constants';
+
+describe('Placeholder | US phone', () => {
+    beforeEach(() => {
+        cy.visit(DemoPath.Placeholder);
+        cy.get('#phone input')
+            .should('be.visible')
+            .first()
+            .should('have.value', '')
+            .focus()
+            .should('have.value', '+1 (   ) ___-____')
+            .should('have.prop', 'selectionStart', '+1 ('.length)
+            .should('have.prop', 'selectionEnd', '+1 ('.length)
+            .as('input');
+    });
+
+    describe('basic typing (1 character per keydown)', () => {
+        const tests = [
+            // [Typed value, Masked value, caretIndex]
+            ['2', '+1 (2  ) ___-____', '+1 (2'.length],
+            ['21', '+1 (21 ) ___-____', '+1 (21'.length],
+            ['212', '+1 (212) ___-____', '+1 (212'.length],
+            ['2125', '+1 (212) 5__-____', '+1 (212) 5'.length],
+            ['21255', '+1 (212) 55_-____', '+1 (212) 55'.length],
+            ['212555', '+1 (212) 555-____', '+1 (212) 555'.length],
+            ['2125552', '+1 (212) 555-2___', '+1 (212) 555-2'.length],
+            ['21255523', '+1 (212) 555-23__', '+1 (212) 555-23'.length],
+            ['212555236', '+1 (212) 555-236_', '+1 (212) 555-236'.length],
+            ['2125552368', '+1 (212) 555-2368', '+1 (212) 555-2368'.length],
+        ] as const;
+
+        tests.forEach(([typed, masked, caretIndex]) => {
+            it(`Type ${typed} => ${masked}`, () => {
+                cy.get('@input')
+                    .type(typed)
+                    .should('have.value', masked)
+                    .should('have.prop', 'selectionStart', caretIndex)
+                    .should('have.prop', 'selectionEnd', caretIndex);
+            });
+        });
+    });
+
+    it('Can type 1 after country code +1', () => {
+        cy.get('@input')
+            .type('1')
+            .should('have.value', '+1 (1  ) ___-____')
+            .should('have.prop', 'selectionStart', '+1 (1'.length)
+            .should('have.prop', 'selectionEnd', '+1 (1'.length);
+    });
+
+    it('cannot erase country code +1', () => {
+        cy.get('@input')
+            .type('{backspace}'.repeat(10))
+            .should('have.value', '+1 (   ) ___-____')
+            .type('{selectAll}{backspace}')
+            .should('have.value', '+1 (   ) ___-____')
+            .type('{selectAll}{del}')
+            .should('have.value', '+1 (   ) ___-____')
+            .should('have.prop', 'selectionStart', '+1'.length)
+            .should('have.prop', 'selectionEnd', '+1'.length);
+    });
+});
---
+++ projects/demo-integrations/cypress/tests/recipes/placeholder/сvc-code.cy.ts
@@ -0,0 +1,87 @@
+import {DemoPath} from '@demo/constants';
+
+describe('Placeholder | CVC code', () => {
+    beforeEach(() => {
+        cy.visit(DemoPath.Placeholder);
+        cy.get('#cvc input')
+            .should('be.visible')
+            .first()
+            .focus()
+            .should('have.value', 'xxx')
+            .should('have.prop', 'selectionStart', 0)
+            .should('have.prop', 'selectionEnd', 0)
+            .as('input');
+    });
+
+    it('Type 1 => 1|xx', () => {
+        cy.get('@input')
+            .type('1')
+            .should('have.value', '1xx')
+            .should('have.prop', 'selectionStart', 1)
+            .should('have.prop', 'selectionEnd', 1);
+    });
+
+    it('Type 12 => 12|x', () => {
+        cy.get('@input')
+            .type('12')
+            .should('have.value', '12x')
+            .should('have.prop', 'selectionStart', 2)
+            .should('have.prop', 'selectionEnd', 2);
+    });
+
+    it('Type 123 => 123|', () => {
+        cy.get('@input')
+            .type('123')
+            .should('have.value', '123')
+            .should('have.prop', 'selectionStart', 3)
+            .should('have.prop', 'selectionEnd', 3);
+    });
+
+    it('12|3 => Backspace => 1|3x', () => {
+        cy.get('@input')
+            .type('123')
+            .type('{leftArrow}{backspace}')
+            .should('have.value', '13x')
+            .should('have.prop', 'selectionStart', 1)
+            .should('have.prop', 'selectionEnd', 1);
+    });
+
+    it('1|3x => Type 0 => 10|3', () => {
+        cy.get('@input')
+            .type('13')
+            .type('{leftArrow}0')
+            .should('have.value', '103')
+            .should('have.prop', 'selectionStart', 2)
+            .should('have.prop', 'selectionEnd', 2);
+    });
+
+    it('1xx => select all => backspace => xxx', () => {
+        cy.get('@input')
+            .type('1')
+            .type('{selectAll}{backspace}')
+            .should('have.value', 'xxx')
+            .should('have.prop', 'selectionStart', 0)
+            .should('have.prop', 'selectionEnd', 0);
+    });
+
+    it('1xx => select all => delete => xxx', () => {
+        cy.get('@input')
+            .type('1')
+            .type('{selectAll}{del}')
+            .should('have.value', 'xxx')
+            .should('have.prop', 'selectionStart', 0)
+            .should('have.prop', 'selectionEnd', 0);
+    });
+
+    it('12x| => Backspace => 12|x', () => {
+        cy.get('@input')
+            .type('12')
+            .type('{rightArrow}')
+            .should('have.prop', 'selectionStart', 3)
+            .should('have.prop', 'selectionEnd', 3)
+            .type('{backspace}')
+            .should('have.value', '12x')
+            .should('have.prop', 'selectionStart', 2)
+            .should('have.prop', 'selectionEnd', 2);
+    });
+});
