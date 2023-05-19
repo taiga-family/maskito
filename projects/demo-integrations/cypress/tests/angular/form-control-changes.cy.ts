36a88fd704ad018b07569b9497263d750a56aaba
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-05-19 10:50:00 +0300

    chore(demo-integrations): new custom Cypress assertion to check Angular form control value (#306)

--- projects/demo-integrations/cypress/tests/angular/form-control-changes.cy.ts
+++ projects/demo-integrations/cypress/tests/angular/form-control-changes.cy.ts
@@ -6,26 +6,23 @@ describe('Angular FormControl and native input have the same values', () => {
 
         cy.get('#demo-content button').should('contain', 'Form value').click();
 
-        cy.get('#demo-content tui-expand pre.t-value')
-            .should('be.visible')
-            .as('controlValue');
-
+        cy.get('#demo-content tui-input').as('inputWrapper');
         cy.get('#demo-content input').should('be.visible').first().focus().as('input');
     });
 
     it('on initialization', () => {
         cy.get('@input').should('have.value', '');
-        cy.get('@controlValue').should('contain', '');
+        cy.get('@inputWrapper').should('have.ngControlValue', '');
     });
 
     it('after input a new character', () => {
         cy.get('@input').type('{moveToStart}').type('2').should('have.value', '2');
-        cy.get('@controlValue').should('contain', '2');
+        cy.get('@inputWrapper').should('have.ngControlValue', '2');
     });
 
     it('after input many new characters', () => {
         cy.get('@input').type('{moveToStart}').type('2359').should('have.value', '23:59');
-        cy.get('@controlValue').should('contain', '23:59');
+        cy.get('@inputWrapper').should('have.ngControlValue', '23:59');
     });
 
     it('after delete via "Backspace"-button', () => {
@@ -35,7 +32,7 @@ describe('Angular FormControl and native input have the same values', () => {
             .type('{backspace}')
             .should('have.value', '23:5');
 
-        cy.get('@controlValue').should('contain', '23:5');
+        cy.get('@inputWrapper').should('have.ngControlValue', '23:5');
     });
 
     it('after delete via "Delete"-button', () => {
@@ -46,7 +43,7 @@ describe('Angular FormControl and native input have the same values', () => {
             .type('{del}')
             .should('have.value', '23:09');
 
-        cy.get('@controlValue').should('contain', '23:09');
+        cy.get('@inputWrapper').should('have.ngControlValue', '23:09');
     });
 
     it('after select all + "Backspace"-button', () => {
@@ -56,7 +53,7 @@ describe('Angular FormControl and native input have the same values', () => {
             .type('{selectall}{backspace}')
             .should('have.value', '');
 
-        cy.get('@controlValue').should('contain', '');
+        cy.get('@inputWrapper').should('have.ngControlValue', '');
     });
 
     it('after select all + "Delete"-button', () => {
@@ -66,6 +63,6 @@ describe('Angular FormControl and native input have the same values', () => {
             .type('{selectall}{del}')
             .should('have.value', '');
 
-        cy.get('@controlValue').should('contain', '');
+        cy.get('@inputWrapper').should('have.ngControlValue', '');
     });
 });
