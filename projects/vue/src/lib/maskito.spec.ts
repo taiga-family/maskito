4211256eab2346130a9d2779cbd218df862f7b8a
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-06-02 18:04:18 +0300

    ci: drop extra peer dependency from `@maskito/vue` build

--- projects/vue/src/lib/maskito.spec.ts
+++ projects/vue/src/lib/maskito.spec.ts
@@ -1,18 +1,28 @@
-import {maskitoNumberOptionsGenerator} from '@maskito/kit';
 import {maskito} from '@maskito/vue';
 import {mount} from '@vue/test-utils';
 
 describe('Maskito Vue package', () => {
+    const options = {
+        mask: [
+            ...Array(4).fill(/\d/),
+            ' ',
+            ...Array(4).fill(/\d/),
+            ' ',
+            ...Array(4).fill(/\d/),
+            ' ',
+            ...Array(4).fill(/\d/),
+        ],
+    };
     const component = {
         template: '<input v-model="value" v-maskito="options" />',
         directives: {maskito},
         data: () => ({
-            value: '1234567',
-            options: maskitoNumberOptionsGenerator(),
+            value: '1234567890123456',
+            options: options,
         }),
     };
 
     test('formats text', () => {
-        expect(mount(component).find('input').element.value).toBe(`1\u00A0234\u00A0567`);
+        expect(mount(component).find('input').element.value).toBe(`1234 5678 9012 3456`);
     });
 });
