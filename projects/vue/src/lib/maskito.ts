6f662c2b5682cb814ba4f5851cb90081b3a8bbe6
waterplea <alexander@inkin.ru>
2023-06-14 20:21:25 +0400

    feat(vue): support async predicate

--- projects/vue/src/lib/maskito.ts
+++ projects/vue/src/lib/maskito.ts
@@ -2,18 +2,32 @@ import {
     Maskito,
     MASKITO_DEFAULT_ELEMENT_PREDICATE,
     MaskitoElementPredicate,
+    MaskitoElementPredicateAsync,
     MaskitoOptions,
 } from '@maskito/core';
 import {ObjectDirective} from 'vue';
 
 const teardown = new Map<HTMLElement, Maskito>();
+const predicates = new Map<
+    HTMLElement,
+    MaskitoElementPredicate | MaskitoElementPredicateAsync
+>();
 
-function update(
+async function update(
     element: HTMLElement,
-    options: MaskitoOptions & {elementPredicate?: MaskitoElementPredicate},
-): void {
+    options: MaskitoOptions & {
+        elementPredicate?: MaskitoElementPredicate | MaskitoElementPredicateAsync;
+    },
+): Promise<void> {
     const predicate = options.elementPredicate ?? MASKITO_DEFAULT_ELEMENT_PREDICATE;
-    const predicateResult = predicate(element);
+
+    predicates.set(element, predicate);
+
+    const predicateResult = await predicate(element);
+
+    if (predicates.get(element) !== predicate) {
+        return;
+    }
 
     teardown.get(element)?.destroy();
     teardown.set(element, new Maskito(predicateResult, options));
@@ -22,6 +36,7 @@ function update(
 function unmount(element: HTMLElement): void {
     teardown.get(element)?.destroy();
     teardown.delete(element);
+    predicates.delete(element);
 }
 
 export const maskito: ObjectDirective<
@@ -29,10 +44,10 @@ export const maskito: ObjectDirective<
     MaskitoOptions & {elementPredicate?: MaskitoElementPredicate}
 > = {
     unmounted: element => unmount(element),
-    mounted: (element, {value}) => update(element, value),
-    updated: (element, {value, oldValue}) => {
+    mounted: async (element, {value}) => update(element, value),
+    updated: async (element, {value, oldValue}) => {
         if (value !== oldValue) {
-            update(element, value);
+            await update(element, value);
         }
     },
 };
