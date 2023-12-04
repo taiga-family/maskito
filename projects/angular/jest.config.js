34ffdd3ea7f39afc873ea9634f7bf6f840a84dde
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-12-04 17:01:22 +0300

    feat!: update to Angular 13 (#710)

--- projects/angular/jest.config.js
+++ projects/angular/jest.config.js
@@ -4,8 +4,9 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     coverageDirectory: '../../coverage/angular',
     transform: {
-        '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+        '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
     },
+    transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
     snapshotSerializers: [
         'jest-preset-angular/build/serializers/no-ng-attributes',
         'jest-preset-angular/build/serializers/ng-snapshot',
