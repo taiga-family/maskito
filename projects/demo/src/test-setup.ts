f137f223aa5970c4fa62de71fda748ef76b2f24d
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-12-04 16:28:56 +0300

    chore: drop some redundant configs

--- projects/demo/src/test-setup.ts
+++ projects/demo/src/test-setup.ts
@@ -1,16 +1,3 @@
 import 'jest-preset-angular/setup-jest';
 import '@ng-web-apis/universal/mocks';
 import '@taiga-ui/testing/setup-jest';
-
-import {getTestBed} from '@angular/core/testing';
-import {
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
-
-getTestBed().resetTestEnvironment();
-getTestBed().initTestEnvironment(
-    BrowserDynamicTestingModule,
-    platformBrowserDynamicTesting(),
-    {teardown: {destroyAfterEach: false}},
-);
