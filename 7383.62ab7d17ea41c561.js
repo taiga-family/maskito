8346498cea73bbe992a10f578ecc9ea54b94bd77
nsbarsukov <nikita.s.barsukov@gmail.com>
2023-12-05 17:25:53 +0000

    Deploying to gh-pages from @ taiga-family/maskito@c8eeabb058be29e8a42f4c85fba0c850b797f873 🚀

---
+++ 7383.62ab7d17ea41c561.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[7383],{7383:t=>{t.exports='<input\n    [formControl]="control"\n    [maskito]="maskito"\n/>\n<button (click)="setValue()">Set 12345.6789</button>\n'}}]);
