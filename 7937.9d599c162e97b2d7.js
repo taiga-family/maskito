1df07ab7a5054bdad69eda6815b0580f2f5e2aaa
nsbarsukov <nikita.s.barsukov@gmail.com>
2023-12-12 09:34:46 +0000

    Deploying to gh-pages from @ taiga-family/maskito@b9e63f3e145ab7471f75b74edb5b6d76408cbcfa 🚀

---
+++ 7937.9d599c162e97b2d7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[7937],{7937:n=>{n.exports="```ts\nimport {Component} from '@angular/core';\nimport {MaskitoDirective} from '@maskito/angular';\n\n@Component({\n  // ...\n  imports: [\n    MaskitoDirective,\n    // ...\n  ],\n})\nexport class YourComponent {}\n```\n"}}]);
