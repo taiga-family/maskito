3cf46250e91f21944129aac5d8c93bb858c0898c
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-12-13 14:11:49 +0300

    chore: update cypress tests

--- projects/demo/src/pages/cypress/cypress.module.ts
+++ projects/demo/src/pages/cypress/cypress.module.ts
@@ -11,7 +11,7 @@ import {CypressDocPageComponent} from './cypress.component';
 import {TestDocExample1} from './examples/1-predicate/component';
 import {TestDocExample2} from './examples/2-native-max-length/component';
 import {TestDocExample3} from './examples/3-mirrored-prefix-postfix/component';
-import {TestDocExample4, TestPipe4} from './examples/4-runtime-postfix-changes/component';
+import {TestDocExample4} from './examples/4-runtime-postfix-changes/component';
 import {TestDocExample5} from './examples/5-react-async-predicate/angular-wrapper';
 import {TestDocExample6} from './examples/6-multi-character-prefix/component';
 
@@ -25,14 +25,13 @@ import {TestDocExample6} from './examples/6-multi-character-prefix/component';
         TuiGroupModule,
         TuiAddonDocModule,
         RouterModule.forChild(tuiGenerateRoutes(CypressDocPageComponent)),
+        TestDocExample4,
     ],
     declarations: [
         CypressDocPageComponent,
         TestDocExample1,
         TestDocExample2,
         TestDocExample3,
-        TestDocExample4,
-        TestPipe4,
         TestDocExample5,
         TestDocExample6,
     ],
