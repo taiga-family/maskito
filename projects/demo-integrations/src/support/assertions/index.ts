cccbcafcd90194fad140a7dd62e5552035aa70ac
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2023-12-29 11:26:38 +0300

    chore(demo-integrations): custom commands and assertions refactor (#839)

--- projects/demo-integrations/src/support/assertions/index.ts
+++ projects/demo-integrations/src/support/assertions/index.ts
@@ -1,1 +1,17 @@
-export * from './have-ng-control-value';
+import {haveNgControlValueAssertion} from './have-ng-control-value';
+
+declare global {
+    namespace Cypress {
+        interface Chainer<Subject> {
+            /**
+             * Assertion that checks if given subject has Angular form control with specified value
+             *
+             * @example
+             * cy.get('tui-input').should('have.ngControlValue', '123')
+             * */
+            (chainer: 'have.ngControlValue'): Chainable<Subject>;
+        }
+    }
+}
+
+chai.use(haveNgControlValueAssertion);
