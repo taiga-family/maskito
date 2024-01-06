9a9426907c593808a18a1b8dd652a24508b5aec2
Soshi Homma <rikusen0335@gmail.com>
2024-01-06 20:45:34 +0900

    chore: lint

--- projects/kit/src/lib/masks/number/processors/create-fullwidth-to-halfwidth-preprocessor.ts
+++ projects/kit/src/lib/masks/number/processors/create-fullwidth-to-halfwidth-preprocessor.ts
@@ -1,24 +1,24 @@
-import { MaskitoPreprocessor } from '@maskito/core';
+import {MaskitoPreprocessor} from '@maskito/core';
 
 /**
  * Convert full width numbers like １, ２ to half width numbers 1, 2
- * 
+ *
  * 全角の１、２ののような数字を、半角の1、2のように直すプリプロセッサ
  */
 export function createFullWidthToHalfWidthPreprocessor(): MaskitoPreprocessor {
-  return ({ elementState, data }) => {
-    const { value, selection } = elementState;
+    return ({elementState, data}) => {
+        const {value, selection} = elementState;
 
-    return {
-      elementState: {
-        selection,
-        value: value.replace(/[０-９]/g, (s) =>
-          String.fromCharCode(s.charCodeAt(0) - 0xfee0)
-        ),
-      },
-      data: data.replace(/[０-９]/g, (s) =>
-        String.fromCharCode(s.charCodeAt(0) - 0xfee0)
-      ),
+        return {
+            elementState: {
+                selection,
+                value: value.replace(/[０-９]/g, s =>
+                    String.fromCharCode(s.charCodeAt(0) - 0xfee0),
+                ),
+            },
+            data: data.replace(/[０-９]/g, s =>
+                String.fromCharCode(s.charCodeAt(0) - 0xfee0),
+            ),
+        };
     };
-  };
 }
