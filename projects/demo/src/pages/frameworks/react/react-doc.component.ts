3b8733b33bb6526bb2818c2f6eb689a9a7effe96
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-01-11 09:47:20 +0300

    chore(demo): delete `@ts-nocheck` (no more required after Vue@3.4.0 update) (#885)

--- projects/demo/src/pages/frameworks/react/react-doc.component.ts
+++ projects/demo/src/pages/frameworks/react/react-doc.component.ts
@@ -2,8 +2,6 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {DemoPath} from '@demo/constants';
 import {TuiRawLoaderContent} from '@taiga-ui/addon-doc';
 
-const DROP_TS_NO_CHECK_REG = /\/\/\s@ts-nocheck[^\n]+\n/;
-
 @Component({
     selector: 'react-doc-page',
     templateUrl: './react-doc.template.html',
@@ -16,11 +14,7 @@ export class ReactDocPageComponent {
     );
 
     readonly elementPredicateExample: Record<string, TuiRawLoaderContent> = {
-        'index.tsx': import('./examples/2-element-predicate/index.tsx?raw').then(m => ({
-            // See: https://github.com/vuejs/core/issues/1033#issuecomment-1340309622
-            // TODO: Check if it still required after upgrade Vue to 3.4 (https://github.com/vuejs/core/pull/7958)
-            default: m.default.replace(DROP_TS_NO_CHECK_REG, ''),
-        })),
+        'index.tsx': import('./examples/2-element-predicate/index.tsx?raw'),
         'awesome-input.tsx': import(
             './examples/2-element-predicate/awesome-input.tsx?raw'
         ),
