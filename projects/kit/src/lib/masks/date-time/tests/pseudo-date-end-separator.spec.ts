0175c21c2b4412d7c59e1b60fc003f28384bc671
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-02-08 11:52:27 +0300

    chore: add more unit tests

--- projects/kit/src/lib/masks/date-time/tests/pseudo-date-end-separator.spec.ts
+++ projects/kit/src/lib/masks/date-time/tests/pseudo-date-end-separator.spec.ts
@@ -21,14 +21,24 @@ describe('DateTime (maskitoTransform) | Pseudo date end separators', () => {
     });
 
     it('replaces space with valid date end separator', () => {
+        expect(maskitoTransform('01012000 ', options)).toBe(
+            `01.01.2000${DATE_TIME_SEPARATOR}`,
+        );
         expect(maskitoTransform('01012000 2359', options)).toBe(
             `01.01.2000${DATE_TIME_SEPARATOR}23:59`,
         );
     });
 
     it('replaces comma with valid range separator', () => {
+        expect(maskitoTransform('01012000,', options)).toBe(
+            `01.01.2000${DATE_TIME_SEPARATOR}`,
+        );
         expect(maskitoTransform('01012000,235959999', options)).toBe(
             `01.01.2000${DATE_TIME_SEPARATOR}23:59:59.999`,
         );
+    });
+
+    it('does not add anything if separator does not initially exist', () => {
+        expect(maskitoTransform('01012000', options)).toBe('01.01.2000');
     });
 });
