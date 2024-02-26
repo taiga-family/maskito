0f0017e0520f5cc6b364b6295de52ba1b3eeea55
Stanslav Zaytsev <s.d.zaytsev@tinkoff.ru>
2024-02-26 13:47:26 +0300

    refactor(demo): brought back cypress 1-predicate page

--- projects/demo/src/pages/cypress/cypress.component.ts
+++ projects/demo/src/pages/cypress/cypress.component.ts
@@ -1,6 +1,7 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {TuiAddonDocModule} from '@taiga-ui/addon-doc';
 
+import {TestDocExample1} from './examples/1-predicate/component';
 import {TestDocExample2} from './examples/2-native-max-length/component';
 import {TestDocExample3} from './examples/3-mirrored-prefix-postfix/component';
 import {TestDocExample4} from './examples/4-runtime-postfix-changes/component';
@@ -12,6 +13,7 @@ import {TestDocExample6} from './examples/6-multi-character-prefix/component';
     selector: 'cypress-doc-page',
     imports: [
         TuiAddonDocModule,
+        TestDocExample1,
         TestDocExample2,
         TestDocExample3,
         TestDocExample4,
---
+++ projects/demo/src/pages/cypress/examples/1-predicate/component.ts
@@ -0,0 +1,48 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MaskitoDirective} from '@maskito/angular';
+import {MaskitoElementPredicate, MaskitoOptions} from '@maskito/core';
+import {TuiGroupModule} from '@taiga-ui/core';
+import {TuiInputModule} from '@taiga-ui/kit';
+
+@Component({
+    standalone: true,
+    selector: 'test-doc-example-1',
+    imports: [TuiGroupModule, MaskitoDirective, TuiInputModule, FormsModule],
+    templateUrl: './template.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TestDocExample1 {
+    value = {
+        number: '',
+        name: '',
+    };
+
+    readonly cardMask: MaskitoOptions = {
+        mask: [
+            ...new Array(4).fill(/\d/),
+            ' ',
+            ...new Array(4).fill(/\d/),
+            ' ',
+            ...new Array(4).fill(/\d/),
+            ' ',
+            ...new Array(4).fill(/\d/),
+        ],
+    };
+
+    readonly nameMask: MaskitoOptions = {
+        mask: /^[a-zA-Z\s]+$/,
+        postprocessors: [
+            ({value, selection}) => ({value: value.toUpperCase(), selection}),
+        ],
+    };
+
+    readonly cardPredicate: MaskitoElementPredicate = element =>
+        element.querySelectorAll('input')[0];
+
+    readonly namePredicate: MaskitoElementPredicate = element =>
+        element.querySelectorAll('input')[1];
+
+    readonly asyncPredicate: MaskitoElementPredicate = async element =>
+        Promise.resolve(element.querySelectorAll('input')[0]);
+}
