6c58531a0ea55066d86c4e785feb210f08b5e101
Stanslav Zaytsev <s.d.zaytsev@tinkoff.ru>
2024-02-26 11:40:24 +0300

    feat(demo-integrations): rewrote tests of predicate using TestInput

--- projects/demo-integrations/src/tests/component-testing/predicate/predicate.cy.ts
+++ projects/demo-integrations/src/tests/component-testing/predicate/predicate.cy.ts
@@ -1,26 +1,68 @@
-import {PredicateTestComponent} from './predicate-test.component';
+import {MaskitoOptions} from '@maskito/core';
+
+import {TestInput} from '../utils';
 
 describe('@maskito/angular | Predicate', () => {
-    beforeEach(() => {
-        cy.mount(PredicateTestComponent);
+    const cardMask: MaskitoOptions = {
+        mask: [
+            ...new Array(4).fill(/\d/),
+            ' ',
+            ...new Array(4).fill(/\d/),
+            ' ',
+            ...new Array(4).fill(/\d/),
+            ' ',
+            ...new Array(4).fill(/\d/),
+        ],
+    };
+
+    const nameMask: MaskitoOptions = {
+        mask: /^[a-zA-Z\s]+$/,
+        postprocessors: [
+            ({value, selection}) => ({value: value.toUpperCase(), selection}),
+        ],
+    };
+
+    describe('can detect run-time changes', () => {
+        it('card', () => {
+            cy.mount(TestInput, {
+                componentProperties: {
+                    maskitoOptions: cardMask,
+                    maskitoElementPredicate: element =>
+                        element.querySelectorAll('input')[0],
+                },
+            });
+
+            cy.get('input')
+                .type('12341234abcd12341234')
+                .should('have.value', '1234 1234 1234 1234');
+        });
+
+        it('name', () => {
+            cy.mount(TestInput, {
+                componentProperties: {
+                    maskitoOptions: nameMask,
+                    maskitoElementPredicate: element =>
+                        element.querySelectorAll('input')[0],
+                },
+            });
+
+            cy.get('input').type('12341234abcd12341234').should('have.value', 'ABCD');
+        });
     });
-    it('can detect run-time changes', () => {
-        cy.get('#predicate input')
-            .first()
-            .focus()
-            .type('12341234abcd12341234')
-            .should('have.value', '1234 1234 1234 1234');
-        cy.get('#predicate input')
-            .eq(1)
-            .focus()
-            .type('12341234abcd12341234')
-            .should('have.value', 'ABCD');
-    });
-    it('supports asynchronous predicate', () => {
-        cy.get('#async-predicate input').as('card');
-        cy.get('@card')
-            .focus()
-            .type('12341234abcd12341234')
-            .should('have.value', '1234 1234 1234 1234');
+    describe('supports asynchronous predicate', () => {
+        it('card', () => {
+            cy.mount(TestInput, {
+                componentProperties: {
+                    maskitoOptions: cardMask,
+                    maskitoElementPredicate: async element =>
+                        Promise.resolve(element.querySelectorAll('input')[0]),
+                },
+            });
+            cy.get('input').as('card');
+            cy.get('@card')
+                .focus()
+                .type('12341234abcd12341234')
+                .should('have.value', '1234 1234 1234 1234');
+        });
     });
 });
