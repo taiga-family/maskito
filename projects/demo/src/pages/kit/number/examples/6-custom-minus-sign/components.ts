4ded341bf2703cee1a5b061dfc0a1afd40fb3f90
Stanslav Zaytsev <s.d.zaytsev@tinkoff.ru>
2024-03-04 15:49:44 +0300

    docs(demo): changed minus sign of the example, and refactored example componenet

--- projects/demo/src/pages/kit/number/examples/6-custom-minus-sign/components.ts
+++ projects/demo/src/pages/kit/number/examples/6-custom-minus-sign/components.ts
@@ -1,12 +1,12 @@
 import {Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MaskitoDirective} from '@maskito/angular';
-import {MaskitoOptions} from '@maskito/core';
 import {TuiTextfieldControllerModule} from '@taiga-ui/core';
 import {TuiInputModule} from '@taiga-ui/kit';
 
 import {getMaskitoOptions} from './mask';
 
+const mask = getMaskitoOptions('-');
 @Component({
     standalone: true,
     selector: 'number-mask-doc-example-6',
@@ -23,16 +23,12 @@ import {getMaskitoOptions} from './mask';
         >
             <input
                 tuiTextfield
-                [maskito]="getMaskOptions(minusSign)"
+                [maskito]="options"
             />
         </tui-input>
     `,
 })
 export class NumberMaskDocExample6 {
-    protected value = '\u30FC42';
-    protected minusSign = '\u30FC';
-
-    protected getMaskOptions(minusSign: string): MaskitoOptions {
-        return getMaskitoOptions(minusSign);
-    }
+    protected value = '-42';
+    protected options = mask;
 }
