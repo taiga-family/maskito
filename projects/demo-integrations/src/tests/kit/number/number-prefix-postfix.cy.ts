6f6275a69fd2130ca8c443cc89dee979637d03e9
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-03-11 10:08:28 +0300

    fix(demo-integrations): resoleved conflict in number-prefix-postfix.ct.ts

--- projects/demo-integrations/src/tests/kit/number/number-prefix-postfix.cy.ts
+++ projects/demo-integrations/src/tests/kit/number/number-prefix-postfix.cy.ts
@@ -222,45 +222,6 @@ describe('Number | Prefix & Postfix', () => {
         });
     });
 
-    describe('runtime changes of postfix', () => {
-        beforeEach(() => {
-            cy.visit(DemoPath.Cypress);
-            cy.get('#runtime-postfix-changes input')
-                .focus()
-                .should('have.value', '1 year')
-                .as('input');
-        });
-
-        it('1| year => Type 0 => 10| years', () => {
-            cy.get('@input')
-                .type('{moveToStart}{rightArrow}')
-                .type('0')
-                .should('have.value', '10 years')
-                .should('have.prop', 'selectionStart', '10'.length)
-                .should('have.prop', 'selectionEnd', '10'.length);
-        });
-
-        it('10| years => Backspace => 1| year', () => {
-            cy.get('@input')
-                .type('{moveToStart}{rightArrow}')
-                .type('0')
-                .should('have.value', '10 years')
-                .type('{backspace}')
-                .should('have.value', '1 year')
-                .should('have.prop', 'selectionStart', '1'.length)
-                .should('have.prop', 'selectionEnd', '1'.length);
-        });
-
-        it('select all + delete', () => {
-            cy.get('@input')
-                .should('have.value', '1 year')
-                .type('{selectAll}{del}')
-                .should('have.value', '')
-                .should('have.prop', 'selectionStart', 0)
-                .should('have.prop', 'selectionEnd', 0);
-        });
-    });
-
     describe('prefix ends with the same character as postfix starts', () => {
         const prefix = 'lbs.​'; // padded with zero-width space
 
