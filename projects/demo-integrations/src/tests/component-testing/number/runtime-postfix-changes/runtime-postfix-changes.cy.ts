d18df64acc81a8eafaefa1140f5f628323b1e431
Stanislav Zaycev <102649815+KrollikRoddzer@users.noreply.github.com>
2024-03-11 11:35:56 +0300

    refactor(demo-integrations): rewriting e2e tests to CT(runtime-prefix-changes tests)  (#1131)

---
+++ projects/demo-integrations/src/tests/component-testing/number/runtime-postfix-changes/runtime-postfix-changes.cy.ts
@@ -0,0 +1,37 @@
+import {MultiTestComponent} from './multi-test.component';
+
+describe('Number | runtime changes of postfix', () => {
+    beforeEach(() => {
+        cy.mount(MultiTestComponent);
+        cy.get('input').focus().should('have.value', '1 year').as('input');
+    });
+
+    it('1| year => Type 0 => 10| years', () => {
+        cy.get('@input')
+            .type('{moveToStart}{rightArrow}')
+            .type('0')
+            .should('have.value', '10 years')
+            .should('have.prop', 'selectionStart', '10'.length)
+            .should('have.prop', 'selectionEnd', '10'.length);
+    });
+
+    it('10| years => Backspace => 1| year', () => {
+        cy.get('@input')
+            .type('{moveToStart}{rightArrow}')
+            .type('0')
+            .should('have.value', '10 years')
+            .type('{backspace}')
+            .should('have.value', '1 year')
+            .should('have.prop', 'selectionStart', '1'.length)
+            .should('have.prop', 'selectionEnd', '1'.length);
+    });
+
+    it('select all + delete', () => {
+        cy.get('@input')
+            .should('have.value', '1 year')
+            .type('{selectAll}{del}')
+            .should('have.value', '')
+            .should('have.prop', 'selectionStart', 0)
+            .should('have.prop', 'selectionEnd', 0);
+    });
+});
