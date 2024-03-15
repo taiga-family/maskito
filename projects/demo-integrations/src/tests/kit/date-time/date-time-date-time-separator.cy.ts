6661ef8258269eee0c41569f72ae67eb498f2f09
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-03-15 10:01:17 +0300

    refactor(demo-integrations): refactored date time separator tests

--- projects/demo-integrations/src/tests/kit/date-time/date-time-date-time-separator.cy.ts
+++ projects/demo-integrations/src/tests/kit/date-time/date-time-date-time-separator.cy.ts
@@ -1,5 +1,4 @@
 import {DemoPath} from '@demo/constants';
-import {BROWSER_SUPPORTS_REAL_EVENTS} from 'projects/demo-integrations/src/support/constants';
 
 describe('DateTime | dateTimeSeparator', () => {
     const dateTimeSeparators = [':', ';_', '_-_', '_at_'];
@@ -9,31 +8,27 @@ describe('DateTime | dateTimeSeparator', () => {
             type: string;
             result: string;
             date: string;
-            times: number;
             timeMode: string;
         }> = [
             {
                 type: '522004341',
                 result: `05.02.2004${dateTimeSeparator}03:41`,
                 date: '05.02.2004',
-                times: 4,
                 timeMode: 'HH:MM',
             },
             {
                 type: '52200434111',
                 result: `05.02.2004${dateTimeSeparator}03:41:11`,
                 date: '05.02.2004',
-                times: 6,
                 timeMode: 'HH:MM:SS',
             },
             {
                 type: '52200434111111',
                 result: `05.02.2004${dateTimeSeparator}03:41:11:111`,
                 date: '05.02.2004',
-                times: 9,
                 timeMode: 'HH:MM:SS:MSS',
             },
-        ] as const;
+        ];
 
         describe(`correctly applies "${dateTimeSeparator}" as dateTimeSeparator`, () => {
             dates.forEach(date => {
@@ -48,31 +43,17 @@ describe('DateTime | dateTimeSeparator', () => {
                         .as('input');
                 });
 
-                it(
-                    `${date.type} => ${date.result} => {backspace} * ${date.times} => ${date.date}`,
-                    BROWSER_SUPPORTS_REAL_EVENTS,
-                    () => {
-                        cy.get('@input')
-                            .type(date.type)
-                            .should('have.value', date.result)
-                            .realPress(getBackspaces(date.times));
+                it(`${date.type} => ${date.result} => {backspace} * ${date.timeMode.replaceAll(':', '').length} => ${date.date}`, () => {
+                    cy.get('@input')
+                        .type(date.type)
+                        .should('have.value', date.result)
+                        .type(
+                            `${'{backspace}'.repeat(date.timeMode.replaceAll(':', '').length)}`,
+                        );
 
-                        cy.get('@input')
-
-                            .should('have.value', '05.02.2004');
-                    },
-                );
+                    cy.get('@input').should('have.value', '05.02.2004');
+                });
             });
         });
     });
 });
-
-function getBackspaces(times: number): any[] {
-    const result = [];
-
-    for (let i = 0; i < times; i += 1) {
-        result.push('{backspace}');
-    }
-
-    return result;
-}
