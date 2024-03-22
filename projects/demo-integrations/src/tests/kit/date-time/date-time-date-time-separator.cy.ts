9a451556880a2a5cbf39ec534aa3597100eaaf69
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-03-22 11:14:56 +0300

    refactor(demo-integrations): refactored e2e tests for dateTimeSeparator

--- projects/demo-integrations/src/tests/kit/date-time/date-time-date-time-separator.cy.ts
+++ projects/demo-integrations/src/tests/kit/date-time/date-time-date-time-separator.cy.ts
@@ -5,56 +5,57 @@ describe('DateTime | dateTimeSeparator', () => {
     const dateTimeSeparators = [':', ';_', '_-_', '_at_'];
 
     dateTimeSeparators.forEach(dateTimeSeparator => {
-        const dates: Array<{
-            type: string;
-            result: string;
-            date: string;
+        const testCases: Array<{
+            typedDigits: string;
+            formattedDate: string;
+            formattedValue: string;
             timeMode: MaskitoTimeMode;
         }> = [
             {
-                type: '522004341',
-                result: `05.02.2004${dateTimeSeparator}03:41`,
-                date: '05.02.2004',
+                typedDigits: '522004341',
+                formattedDate: `05.02.2004${dateTimeSeparator}03:41`,
+                formattedValue: '05.02.2004',
                 timeMode: 'HH:MM',
             },
             {
-                type: '52200434111',
-                result: `05.02.2004${dateTimeSeparator}03:41:11`,
-                date: '05.02.2004',
+                typedDigits: '233123434111',
+                formattedDate: `23.03.1234${dateTimeSeparator}03:41:11`,
+                formattedValue: '23.03.1234',
                 timeMode: 'HH:MM:SS',
             },
             {
-                type: '52200434111111',
-                result: `05.02.2004${dateTimeSeparator}03:41:11.111`,
-                date: '05.02.2004',
+                typedDigits: '69200734111111',
+                formattedDate: `06.09.2007${dateTimeSeparator}03:41:11.111`,
+                formattedValue: '06.09.2007',
                 timeMode: 'HH:MM:SS.MSS',
             },
         ];
 
         describe(`correctly applies "${dateTimeSeparator}" as dateTimeSeparator`, () => {
-            dates.forEach(date => {
-                beforeEach(() => {
-                    cy.visit(
-                        `/${DemoPath.DateTime}/API?dateTimeSeparator=${encodeURIComponent(dateTimeSeparator)}&timeMode=${encodeURIComponent(date.timeMode)}`,
-                    );
-                    cy.get('#demo-content input')
-                        .should('be.visible')
-                        .first()
-                        .focus()
-                        .as('input');
-                });
+            testCases.forEach(
+                ({typedDigits, formattedDate, formattedValue, timeMode}) => {
+                    const timeDigitsCount = timeMode.replaceAll(/[:.]/g, '').length;
 
-                it(`${date.type} => ${date.result} => {backspace} * ${date.timeMode.replaceAll(/[:.]/g, '').length} => ${date.date}`, () => {
-                    cy.get('@input')
-                        .type(date.type)
-                        .should('have.value', date.result)
-                        .type(
-                            `${'{backspace}'.repeat(date.timeMode.replaceAll(/[:.]/g, '').length)}`,
+                    beforeEach(() => {
+                        cy.visit(
+                            `/${DemoPath.DateTime}/API?dateTimeSeparator=${encodeURIComponent(dateTimeSeparator)}&timeMode=${encodeURIComponent(timeMode)}`,
                         );
+                        cy.get('#demo-content input')
+                            .should('be.visible')
+                            .first()
+                            .focus()
+                            .as('input');
+                    });
 
-                    cy.get('@input').should('have.value', '05.02.2004');
-                });
-            });
+                    it(`${typedDigits} => ${formattedDate} => {backspace} * ${timeDigitsCount} => ${formattedValue}`, () => {
+                        cy.get('@input')
+                            .type(typedDigits)
+                            .should('have.value', formattedDate)
+                            .type(`${'{backspace}'.repeat(timeDigitsCount)}`)
+                            .should('have.value', formattedValue);
+                    });
+                },
+            );
         });
     });
 });
