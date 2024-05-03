508da8d082b867d51df060c67eb88ef090dd114e
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-05-03 14:13:44 +0300

    chore: fix cases with prefix & postfix

--- projects/kit/src/lib/masks/number/processors/not-empty-integer-part-preprocessor.ts
+++ projects/kit/src/lib/masks/number/processors/not-empty-integer-part-preprocessor.ts
@@ -1,6 +1,6 @@
 import type {MaskitoPreprocessor} from '@maskito/core';
 
-import {escapeRegExp, extractAffixes} from '../../../utils';
+import {clamp, escapeRegExp, extractAffixes} from '../../../utils';
 
 /**
  * It pads integer part with zero if user types decimal separator (for empty input).
@@ -23,22 +23,24 @@ export function createNotEmptyIntegerPartPreprocessor({
 
     return ({elementState, data}) => {
         const {value, selection} = elementState;
-        const {cleanValue} = extractAffixes(value, {
+        const {cleanValue, extractedPrefix} = extractAffixes(value, {
             prefix,
             postfix,
         });
         const [from, to] = selection;
+        const cleanFrom = clamp(from - extractedPrefix.length, 0, cleanValue.length);
+        const cleanTo = clamp(to - extractedPrefix.length, 0, cleanValue.length);
 
         if (
             precision <= 0 ||
-            cleanValue.slice(0, from).includes(decimalSeparator) ||
-            cleanValue.slice(to).includes(decimalSeparator) ||
+            cleanValue.slice(0, cleanFrom).includes(decimalSeparator) ||
+            cleanValue.slice(cleanTo).includes(decimalSeparator) ||
             !data.match(startWithDecimalSepRegExp)
         ) {
             return {elementState, data};
         }
 
-        const digitsBeforeCursor = cleanValue.slice(0, from).match(/\d+/);
+        const digitsBeforeCursor = cleanValue.slice(0, cleanFrom).match(/\d+/);
 
         return {
             elementState,
