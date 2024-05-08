f529eccf22ad44b48b667cda62cea26c4aff5d89
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-05-08 09:10:17 +0300

    test(demo-integrations): date-time-time-segmants

---
+++ projects/demo-integrations/src/tests/kit/date-time/date-time-time-step.cy.ts
@@ -0,0 +1,112 @@
+import {DemoPath} from '@demo/constants';
+
+describe('DateTime | timeStep', () => {
+    describe('yy/mm;HH:MM:SS.MSS', () => {
+        describe('timeStep = 1, initial state = 22.12;', () => {
+            beforeEach(() => {
+                cy.visit(
+                    `/${DemoPath.DateTime}/API?dateTimeSeparator=;&dateMode=yy%2Fmm&timeStep=1&timeMode=HH:MM:SS.MSS`,
+                );
+                cy.get('#demo-content input')
+                    .should('be.visible')
+                    .first()
+                    .focus()
+                    .as('input');
+
+                cy.get('@input')
+                    .type('2212.')
+                    .should('have.value', '22.12;')
+                    .should('have.a.prop', 'selectionStart', '22.12;'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;'.length);
+            });
+
+            it('type {downArrow} => 22.12;|23 => type {rightArrow}{downArrow} => 22.12;2|2 => type {rightArrow}{upArrow}*12 => 22.12;10|', () => {
+                cy.get('@input')
+                    .type('{downArrow}')
+                    .should('have.value', '22.12;23')
+                    .should('have.a.prop', 'selectionStart', '22.12;'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;'.length)
+                    .type('{rightArrow}{downArrow}')
+                    .should('have.value', '22.12;22')
+                    .should('have.a.prop', 'selectionStart', '22.12;2'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;2'.length)
+                    .type(`{rightArrow}${'{upArrow}'.repeat(12)}`)
+                    .should('have.value', '22.12;10')
+                    .should('have.a.prop', 'selectionStart', '22.12;10'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;10'.length);
+            });
+
+            it('type 12: => 22.12;12:| => type {upArrow} => 22.12;12:|01 => type {rightArrow}{upArrow} * 9 => 22.12;12:1|0 => type {rightArrow}{downArrow}*34 => 22.12;12:45|', () => {
+                cy.get('@input')
+                    .type('12:')
+                    .should('have.value', '22.12;12:')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:'.length)
+                    .type('{upArrow}')
+                    .should('have.value', '22.12;12:01')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:'.length)
+                    .type(`{rightArrow}${'{upArrow}'.repeat(9)}`)
+                    .should('have.value', '22.12;12:10')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:1'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:1'.length)
+                    .type(`{rightArrow}${'{downArrow}'.repeat(34)}`)
+                    .should('have.value', '22.12;12:36')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:36'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:36'.length);
+            });
+
+            it('type 12:10: => 22.12;12:10:| => type {downArrow}*6 => 22.12;12:10:|54 => type ({rightArrow}{arrowUp}) * 2 => 22.12;12:10:56|', () => {
+                cy.get('@input')
+                    .type('12:10:')
+                    .should('have.value', '22.12;12:10:')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:10:'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:10:'.length)
+                    .type('{downArrow}'.repeat(6))
+                    .should('have.value', '22.12;12:10:54')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:10:'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:10:'.length)
+                    .type('{rightArrow}{upArrow}'.repeat(2))
+                    .should('have.value', '22.12;12:10:56')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:10:56'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:10:56'.length);
+            });
+
+            it('type 213212. => 22.12;21:32:12.| => type ({upArrow}{rightArrow}) * 3 + {downArrow} => 22.12:21:32:12.002|', () => {
+                cy.get('@input')
+                    .type('213212.')
+                    .should('have.value', '22.12;21:32:12.')
+                    .should('have.a.prop', 'selectionStart', '22.12;21:32:12.'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;21:32:12.'.length)
+                    .type(`${'{upArrow}{rightArrow}'.repeat(3)}{downArrow}`)
+                    .should('have.value', '22.12;21:32:12.002')
+                    .should('have.a.prop', 'selectionStart', '22.12;21:32:12.002'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;21:32:12.002'.length);
+            });
+
+            it('type 213212. => 22.12;21:32:12.| => type ({downArrow}{rightArrow}) * 3 + {downArrow} => 22.12:21:32:12.995|', () => {
+                cy.get('@input')
+                    .type('213212.')
+                    .should('have.value', '22.12;21:32:12.')
+                    .should('have.a.prop', 'selectionStart', '22.12;21:32:12.'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;21:32:12.'.length)
+                    .type(`${'{downArrow}{rightArrow}'.repeat(3)}{downArrow}`)
+                    .should('have.value', '22.12;21:32:12.996')
+                    .should('have.a.prop', 'selectionStart', '22.12;21:32:12.996'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;21:32:12.996'.length);
+            });
+
+            it('should affect only time segments', () => {
+                cy.get('@input')
+                    .type('123456111')
+                    .should('have.value', '22.12;12:34:56.111')
+                    .should('have.a.prop', 'selectionStart', '22.12;12:34:56.111'.length)
+                    .should('have.a.prop', 'selectionEnd', '22.12;12:34:56.111'.length)
+                    .type('{upArrow}{leftArrow}'.repeat('22.12;12:34:56.111'.length))
+                    .should('have.value', '22.12;15:37:59.115')
+                    .should('have.a.prop', 'selectionStart', 0)
+                    .should('have.a.prop', 'selectionEnd', 0);
+            });
+        });
+    });
+});
