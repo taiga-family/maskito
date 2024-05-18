f3914afacffa9a54cf340e3e2f04ff241b9ec71f
Stanslav Zaytsev <krollikroddzer@mail.ru>
2024-05-18 09:11:14 +0300

    refactor(kit): parseTimeString

--- projects/kit/src/lib/utils/time/parse-time-string.ts
+++ projects/kit/src/lib/utils/time/parse-time-string.ts
@@ -1,5 +1,12 @@
 import type {MaskitoTimeMode, MaskitoTimeSegments} from '../../types';
 
+const SEGMENT_FULL_NAME: Record<string, keyof MaskitoTimeSegments> = {
+    HH: 'hours',
+    MM: 'minutes',
+    SS: 'seconds',
+    MSS: 'milliseconds',
+};
+
 /**
  * @param timeString can be with/without fixed characters
  */
@@ -9,32 +16,15 @@ export function parseTimeString(
 ): Partial<MaskitoTimeSegments> {
     const onlyDigits = timeString.replaceAll(/\D+/g, '');
 
-    const sliceIndexes = createSliceIndexes(timeMode);
-
-    const timeSegments = {
-        hours: onlyDigits.slice(...sliceIndexes.hours),
-        minutes: onlyDigits.slice(...sliceIndexes.minutes),
-        seconds: onlyDigits.slice(...sliceIndexes.seconds),
-        milliseconds: onlyDigits.slice(...sliceIndexes.milliseconds),
-    };
+    let offset = 0;
 
     return Object.fromEntries(
-        Object.entries(timeSegments).filter(([_, value]) => Boolean(value)),
+        timeMode.split(/\W/).map(segmentAbbr => {
+            const segmentValue = onlyDigits.slice(offset, offset + segmentAbbr.length);
+
+            offset += segmentAbbr.length;
+
+            return [SEGMENT_FULL_NAME[segmentAbbr], segmentValue];
+        }),
     );
-}
-
-function createSliceIndexes(timeMode: string): MaskitoTimeSegments<[number, number]> {
-    const offset =
-        Number(timeMode.startsWith('MM')) * 2 +
-        Number(timeMode.startsWith('SS')) * 4 +
-        Number(timeMode.startsWith('MSS')) * 6;
-
-    const changeSelection = (index: number): number => Math.max(index - offset, 0);
-
-    return {
-        hours: [changeSelection(0), changeSelection(2)],
-        minutes: [changeSelection(2), changeSelection(4)],
-        seconds: [changeSelection(4), changeSelection(6)],
-        milliseconds: [changeSelection(6), changeSelection(9)],
-    };
 }
