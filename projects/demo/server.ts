dce3a7bffb0dc475f754b6c793790d9a6c8b86cb
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-07-22 10:09:51 +0300

    chore(demo): using standalone API to bootstrap demo application (#1398)

--- projects/demo/server.ts
+++ projects/demo/server.ts
@@ -6,75 +6,45 @@ import {join} from 'node:path';
 import {APP_BASE_HREF} from '@angular/common';
 import {provideLocation, provideUserAgent} from '@ng-web-apis/universal';
 import {ngExpressEngine} from '@nguniversal/express-engine';
-import type {Express} from 'express';
 import express from 'express';
 
-import {AppServerModule} from './src/main.server';
+import bootstrap from './src/main.server';
 
-// The Express app is exported so that it can be used by serverless Functions.
-export function app(): Express {
-    const server = express();
-    const distFolder = join(process.cwd(), 'dist/demo/browser');
-    const indexHtml = existsSync(join(distFolder, 'index.original.html'))
-        ? 'index.original.html'
-        : 'index';
-
-    // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-    server.engine(
-        'html',
-        ngExpressEngine({
-            bootstrap: AppServerModule,
-        }) as any,
-    );
-
-    server.set('view engine', 'html');
-    server.set('views', distFolder);
-
-    // Example Express Rest API endpoints
-    // server.get('/api/**', (req, res) => { });
-    // Serve static files from /browser
-    server.get(
-        '*.*',
-        express.static(distFolder, {
-            maxAge: '1y',
-        }),
-    );
-
-    // All regular routes use the Universal engine
-    server.get('*', (req, res) => {
-        res.render(indexHtml, {
-            req,
-            providers: [
-                {provide: APP_BASE_HREF, useValue: req.baseUrl},
-                provideLocation(req),
-                provideUserAgent(req),
-            ],
-        });
-    });
-
-    return server;
-}
-
-function run(): void {
-    const port = process.env.PORT || 4000;
-
-    // Start up the Node server
-    const server = app();
-
-    server.listen(port, () => {
-        console.info(`Node Express server listening on http://localhost:${port}`);
-    });
-}
-
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule?.filename || '';
 
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-    run();
+    const port = process.env['PORT'] || 4000;
+
+    const server = express();
+    const dist = join(process.cwd(), 'dist/demo/browser');
+
+    server.engine('html', ngExpressEngine({bootstrap}));
+    server.set('view engine', 'html');
+    server.set('views', dist);
+    server.get('*.*', express.static(dist, {maxAge: '1y'}));
+    server.get('*', (req, res) => {
+        res.render(
+            existsSync(join(dist, 'index.original.html'))
+                ? 'index.original.html'
+                : 'index',
+            {
+                req,
+                providers: [
+                    {provide: APP_BASE_HREF, useValue: req.baseUrl},
+                    provideLocation(req),
+                    provideUserAgent(req),
+                ],
+            },
+        );
+    });
+
+    server.listen(port, () =>
+        console.info(`Node Express server listening on http://localhost:${port}`),
+    );
 }
 
 export * from './src/main.server';
+
+export default bootstrap;
--- projects/demo/src/main.server.ts
+++ projects/demo/src/main.server.ts
@@ -1,9 +1,21 @@
-import {enableProdMode} from '@angular/core';
+import type {ApplicationRef} from '@angular/core';
+import {ErrorHandler, importProvidersFrom, mergeApplicationConfig} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {provideServerRendering, ServerModule} from '@angular/platform-server';
+import {UNIVERSAL_PROVIDERS} from '@ng-web-apis/universal';
 
-import {environment} from './environments/environment';
+import {App} from './app/app.component';
+import {APP_CONFIG} from './app/app.config';
+import {ServerErrorHandler} from './app/server-error-handler';
 
-if (environment.production) {
-    enableProdMode();
-}
+const serverConfig = mergeApplicationConfig(APP_CONFIG, {
+    providers: [
+        importProvidersFrom(ServerModule),
+        provideServerRendering(),
+        UNIVERSAL_PROVIDERS,
+        {provide: ErrorHandler, useClass: ServerErrorHandler},
+    ],
+});
 
-export {AppServerModule} from './app/app.server.module';
+export default async (): Promise<ApplicationRef> =>
+    bootstrapApplication(App, serverConfig);
