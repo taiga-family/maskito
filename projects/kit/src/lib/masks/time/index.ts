64d01b60524f01dc715d8b84c41615bc9e1ca5ca
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-08-16 16:16:51 +0300

    refactor: add support for `verbatimModuleSyntax: true` (#1462)

--- projects/kit/src/lib/masks/time/index.ts
+++ projects/kit/src/lib/masks/time/index.ts
@@ -1,3 +1,3 @@
 export {maskitoTimeOptionsGenerator} from './time-mask';
-export {MaskitoTimeParams} from './time-options';
+export type {MaskitoTimeParams} from './time-options';
 export {maskitoParseTime, maskitoStringifyTime} from './utils';
