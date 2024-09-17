2f9910c2270c416d9eedd16db3f6808d51557d38
Maksim Ivanov <omaxphp@yandex.ru>
2024-09-17 17:21:33 +0300

    ci: use `npx syncer` instead (#1629)
    Co-authored-by: splincode <omapxphp@yandex.ru>

--- scripts/helpers/is-maskito-package-name.ts
+++
@@ -1,5 +0,0 @@
-export function isMaskitoPackageName(name = '', ignores: string[] = []): boolean {
-    return (
-        (name?.startsWith('@maskito/') || name === 'maskito') && !ignores.includes(name)
-    );
-}
--- scripts/postbump.ts
+++
@@ -1,8 +0,0 @@
-import {version} from '../package.json';
-import {execute} from './helpers/execute';
-import {syncVersions} from './helpers/sync-versions';
-
-(function main(): void {
-    syncVersions(['./projects', './package-lock.json'], version);
-    execute('git add .');
-})();
