a69c751a834b8a3865702098ba82e793255aa05e
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-09-17 15:07:50 +0300

    chore(demo-integrations): new tests for possible race condition of angular `MaskitoDirective`

--- projects/demo-integrations/src/support/commands/index.ts
+++ projects/demo-integrations/src/support/commands/index.ts
@@ -5,7 +5,10 @@ import {smartTick} from './smart-tick';
 declare global {
     namespace Cypress {
         interface Chainable<Subject> {
-            smartTick(durationMs: number, frequencyMs?: number): Chainable<Subject>;
+            smartTick(
+                durationMs: number,
+                options?: Parameters<typeof smartTick>[2],
+            ): Chainable<Subject>;
         }
     }
 }
