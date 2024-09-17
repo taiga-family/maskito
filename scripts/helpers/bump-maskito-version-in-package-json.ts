57d91fd51d3ccfec895c0ed57e78e8d52a175a81
splincode <omapxphp@yandex.ru>
2024-09-17 12:27:51 +0300

    ci: use `npx syncer` instead

--- scripts/helpers/bump-maskito-version-in-package-json.ts
+++
@@ -1,8 +0,0 @@
-export function bumpMaskitoVersionInPackageJson(
-    packageJson: Record<string, unknown>,
-    version: string,
-): void {
-    if ('version' in packageJson && typeof packageJson['version'] === 'string') {
-        packageJson['version'] = version;
-    }
-}
