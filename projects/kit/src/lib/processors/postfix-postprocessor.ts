44b9c67df1409e042027ed544b1f8d75d968b66b
Andrey Belokopytov <a.v.belokopytov@tbank.ru>
2024-11-25 12:48:03 +0300

    fix: mirror symbols only if mask changes in postfix-postprocessor

--- projects/kit/src/lib/processors/postfix-postprocessor.ts
+++ projects/kit/src/lib/processors/postfix-postprocessor.ts
@@ -26,8 +26,7 @@ export function maskitoPostfixPostprocessorGenerator(
                   '',
               );
               const postfixWasModified =
-                  !value.endsWith(postfix) &&
-                  initialElementState.selection[1] >= initialValueBeforePostfix.length;
+                  initialElementState.selection[1] > initialValueBeforePostfix.length;
               const alreadyExistedValueBeforePostfix = findCommonBeginningSubstr(
                   initialValueBeforePostfix,
                   value,
--- projects/kit/src/lib/utils/extract-affixes.ts
+++ projects/kit/src/lib/utils/extract-affixes.ts
@@ -14,7 +14,16 @@ export function extractAffixes(
     const [extractedPrefix = ''] = value.match(prefixRegExp) ?? [];
     const [extractedPostfix = ''] = value.match(postfixRegExp) ?? [];
 
-    const cleanValue = value.replace(prefixRegExp, '').replace(postfixRegExp, '');
+    if (extractedPrefix || extractedPostfix) {
+        return {
+            extractedPrefix,
+            extractedPostfix,
+            cleanValue: value.slice(
+                extractedPrefix.length,
+                extractedPostfix.length ? -extractedPostfix.length : Infinity,
+            ),
+        };
+    }
 
-    return {extractedPrefix, extractedPostfix, cleanValue};
+    return {extractedPrefix, extractedPostfix, cleanValue: value};
 }
