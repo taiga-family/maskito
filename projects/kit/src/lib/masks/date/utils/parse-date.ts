c26d75dfe9bd3d7b3cdb032257bdc821958ecc45
Stanislav Zaytsev <s.d.zaytsev@tbank.ru>
2025-02-23 19:00:05 +0300

    fix: parse date

--- projects/kit/src/lib/masks/date/utils/parse-date.ts
+++ projects/kit/src/lib/masks/date/utils/parse-date.ts
@@ -1,5 +1,5 @@
 import {DEFAULT_MAX_DATE, DEFAULT_MIN_DATE} from '../../../constants';
-import {parseDateString} from '../../../utils';
+import {parseDateString, segmentsToDate} from '../../../utils';
 import type {MaskitoDateParams} from '../date-mask';
 
 export function maskitoParseDate(
@@ -8,11 +8,7 @@ export function maskitoParseDate(
 ): Date {
     const dateSegments = parseDateString(value, mode);
 
-    const year = Number(dateSegments.year);
-    const month = Number(dateSegments.month);
-    const day = Number(dateSegments.day) ?? 1;
-
-    const parsedDateTime = new Date(year, month, day).getTime();
+    const parsedDateTime = segmentsToDate(dateSegments).getTime();
 
     const minTime = (min ?? DEFAULT_MIN_DATE).getTime();
     const maxTime = (max ?? DEFAULT_MAX_DATE).getTime();
