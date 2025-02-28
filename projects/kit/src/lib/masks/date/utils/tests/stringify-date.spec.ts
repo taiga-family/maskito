756257b37188ea3e4a0364b2fc7c64ee8b88fe98
Stanislav Zaytsev <s.d.zaytsev@tbank.ru>
2025-02-28 11:16:02 +0300

    test: fixx

--- projects/kit/src/lib/masks/date/utils/tests/stringify-date.spec.ts
+++ projects/kit/src/lib/masks/date/utils/tests/stringify-date.spec.ts
@@ -10,29 +10,29 @@ describe('maskitoStringifyDate', () => {
         [
             'dd/mm',
             [
-                {date: new Date('2004-02-01'), text: '01/02'},
+                {date: new Date('2004-02-01'), text: '01.02'},
                 {date: new Date('2012-12-30'), separator: '-', text: '30-12'},
                 {
                     date: new Date('2012-12-30'),
                     max: new Date('2012-12-29'),
-                    text: '29/12',
+                    text: '29.12',
                 },
                 {
                     date: new Date('2012-12-12'),
                     min: new Date('2012-12-13'),
-                    text: '13/12',
+                    text: '13.12',
                 },
             ],
         ],
         [
             'dd/mm/yyyy',
             [
-                {date: new Date('2012-03-25'), text: '25/03/2012'},
+                {date: new Date('2012-03-25'), text: '25.03.2012'},
                 {
                     date: new Date('2024-03-25'),
                     max: new Date('2024-01-31'),
-                    separator: '.',
-                    text: '31.01.2024',
+                    separator: '/',
+                    text: '31/01/2024',
                 },
                 {
                     date: new Date('2024-01-31'),
@@ -45,29 +45,29 @@ describe('maskitoStringifyDate', () => {
         [
             'mm/dd',
             [
-                {date: new Date('2004-02-01'), text: '02/01'},
+                {date: new Date('2004-02-01'), text: '02.01'},
                 {date: new Date('2012-12-30'), separator: '-', text: '12-30'},
                 {
                     date: new Date('2012-12-30'),
                     max: new Date('2012-12-29'),
-                    text: '12/29',
+                    text: '12.29',
                 },
                 {
                     date: new Date('2012-12-12'),
                     min: new Date('2012-12-13'),
-                    text: '12/13',
+                    text: '12.13',
                 },
             ],
         ],
         [
             'mm/dd/yyyy',
             [
-                {date: new Date('2000-10-01'), text: '10/01/2000'},
+                {date: new Date('2000-10-01'), text: '10.01.2000'},
                 {
                     date: new Date('2024-03-25'),
                     max: new Date('2024-01-31'),
-                    separator: '.',
-                    text: '01.31.2024',
+                    separator: '/',
+                    text: '01/31/2024',
                 },
                 {
                     date: new Date('2024-01-31'),
@@ -80,34 +80,34 @@ describe('maskitoStringifyDate', () => {
         [
             'mm/yy',
             [
-                {date: new Date('2000-10-01'), text: '10/00'},
+                {date: new Date('2000-10-01'), text: '10.00'},
                 {
                     date: new Date('2012-10-20'),
                     max: new Date('2012-10-19'),
-                    separator: '.',
-                    text: '10.12',
+                    separator: '/',
+                    text: '10/12',
                 },
                 {
                     date: new Date('2024-01-31'),
                     min: new Date('2024-03-25'),
-                    text: '03/24',
+                    text: '03.24',
                 },
             ],
         ],
         [
             'mm/yyyy',
             [
-                {date: new Date('2000-10-01'), text: '10/2000'},
+                {date: new Date('2000-10-01'), text: '10.2000'},
                 {
                     date: new Date('2012-10-20'),
                     max: new Date('2012-10-19'),
-                    separator: '.',
-                    text: '10.2012',
+                    separator: '/',
+                    text: '10/2012',
                 },
                 {
                     date: new Date('2024-01-31'),
                     min: new Date('2024-03-25'),
-                    text: '03/2024',
+                    text: '03.2024',
                 },
             ],
         ],
@@ -130,7 +130,7 @@ describe('maskitoStringifyDate', () => {
         [
             'yyyy/mm',
             [
-                {date: new Date('2000-10-01'), text: '2000/10'},
+                {date: new Date('2000-10-01'), text: '2000.10'},
                 {
                     date: new Date('2012-10-20'),
                     max: new Date('2012-09-19'),
@@ -140,24 +140,24 @@ describe('maskitoStringifyDate', () => {
                 {
                     date: new Date('2024-01-31'),
                     min: new Date('2024-03-25'),
-                    text: '2024/03',
+                    text: '2024.03',
                 },
             ],
         ],
         [
             'yyyy/mm/dd',
             [
-                {date: new Date('2000-10-01'), text: '2000/10/01'},
+                {date: new Date('2000-10-01'), text: '2000.10.01'},
                 {
                     date: new Date('2024-03-25'),
                     max: new Date('2024-01-31'),
-                    separator: '.',
-                    text: '2024.01.31',
+                    separator: '/',
+                    text: '2024/01/31',
                 },
                 {
                     date: new Date('2024-01-31'),
                     min: new Date('2024-03-25'),
-                    text: '2024/03/25',
+                    text: '2024.03.25',
                 },
             ],
         ],
