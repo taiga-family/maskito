22d4f8314cb4ff1caf940a31f1c404025272b692
Guillermo Orue Marighetti <ggomarighetti@gmail.com>
2025-03-19 13:13:12 +0000

    test(kit): maskitoParseDate now returns possibly null

--- projects/kit/src/lib/masks/date/utils/tests/parse-date.spec.ts
+++ projects/kit/src/lib/masks/date/utils/tests/parse-date.spec.ts
@@ -16,19 +16,19 @@ describe('maskitoParseDate', () => {
         it('should correctly parse 04/29/2012', () => {
             const parsedDate = maskitoParseDate('04/29/2012', params);
 
-            expect(parsedDate.getTime()).toBe(Date.parse('2012-04-29T00:00:00.000'));
+            expect(parsedDate?.getTime()).toBe(Date.parse('2012-04-29T00:00:00.000'));
         });
 
         it('should return min date on 02/04/2004', () => {
             const parsedDate = maskitoParseDate('02/04/2004', params);
 
-            expect(parsedDate.getTime()).toBe(params.min?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.min?.getTime());
         });
 
         it('should return max date on 01/02/2024', () => {
             const parsedDate = maskitoParseDate('01/02/2024', params);
 
-            expect(parsedDate.getTime()).toBe(params.max?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.max?.getTime());
         });
     });
 
@@ -46,37 +46,37 @@ describe('maskitoParseDate', () => {
         it('should return null, 04/mm/yyyy', () => {
             const parsedDate = maskitoParseDate('04', params);
 
-            expect(parsedDate).toBe(null);
+            expect(parsedDate).toBeNull();
         });
 
         it('should return null 02/04/yyyy', () => {
             const parsedDate = maskitoParseDate('02/04', params);
 
-            expect(parsedDate).toBe(null);
+            expect(parsedDate).toBeNull();
         });
 
         it('should return null 01/02/2yyy', () => {
             const parsedDate = maskitoParseDate('01/02/2', params);
 
-            expect(parsedDate).toBe(null);
+            expect(parsedDate).toBeNull();
         });
 
         it('should return null 01/02/20yy', () => {
             const parsedDate = maskitoParseDate('01/02/20', params);
 
-            expect(parsedDate).toBe(null);
+            expect(parsedDate).toBeNull();
         });
 
         it('should return null 01/02/202y', () => {
             const parsedDate = maskitoParseDate('01/02/202', params);
 
-            expect(parsedDate).toBe(null);
+            expect(parsedDate).toBeNull();
         });
 
         it('should return date on 01/02/2024', () => {
             const parsedDate = maskitoParseDate('01/02/2024', params);
 
-            expect(parsedDate.getTime()).toBe(Date.parse('2024-02-01T00:00:00.000'));
+            expect(parsedDate?.getTime()).toBe(Date.parse('2024-02-01T00:00:00.000'));
         });
     });
 
@@ -95,19 +95,19 @@ describe('maskitoParseDate', () => {
         it('should correctly parse 12-31-2020', () => {
             const parsedDate = maskitoParseDate('12-31-2020', params);
 
-            expect(parsedDate.getTime()).toBe(Date.parse('2020-12-31T00:00:00.000'));
+            expect(parsedDate?.getTime()).toBe(Date.parse('2020-12-31T00:00:00.000'));
         });
 
         it('should return min date on 02-27-2004', () => {
             const parsedDate = maskitoParseDate('02-27-2004', params);
 
-            expect(parsedDate.getTime()).toBe(params.min?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.min?.getTime());
         });
 
         it('should return max date on 01/02/2030', () => {
             const parsedDate = maskitoParseDate('01/02/2030', params);
 
-            expect(parsedDate.getTime()).toBe(params.max?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.max?.getTime());
         });
     });
 
@@ -126,19 +126,19 @@ describe('maskitoParseDate', () => {
         it('should correctly parse 02:12', () => {
             const parsedDate = maskitoParseDate('02:12', params);
 
-            expect(parsedDate.getTime()).toBe(Date.parse('2012-02-01T00:00:00.000'));
+            expect(parsedDate?.getTime()).toBe(Date.parse('2012-02-01T00:00:00.000'));
         });
 
         it('should return min date on 01:03', () => {
             const parsedDate = maskitoParseDate('01:03', params);
 
-            expect(parsedDate.getTime()).toBe(params.min?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.min?.getTime());
         });
 
         it('should return max date on 02:31', () => {
             const parsedDate = maskitoParseDate('02:30', params);
 
-            expect(parsedDate.getTime()).toBe(params.max?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.max?.getTime());
         });
     });
 
@@ -157,19 +157,19 @@ describe('maskitoParseDate', () => {
         it('should correctly parse 2012.08', () => {
             const parsedDate = maskitoParseDate('2012.08', params);
 
-            expect(parsedDate.getTime()).toBe(Date.parse('2012-08-01T00:00:00.000'));
+            expect(parsedDate?.getTime()).toBe(Date.parse('2012-08-01T00:00:00.000'));
         });
 
         it('should return min date on 1991.01', () => {
             const parsedDate = maskitoParseDate('1991.01', params);
 
-            expect(parsedDate.getTime()).toBe(params.min?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.min?.getTime());
         });
 
         it('should return max date on 2033.12', () => {
             const parsedDate = maskitoParseDate('2033.12', params);
 
-            expect(parsedDate.getTime()).toBe(params.max?.getTime());
+            expect(parsedDate?.getTime()).toBe(params.max?.getTime());
         });
     });
 });
