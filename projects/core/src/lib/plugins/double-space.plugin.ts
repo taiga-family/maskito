ccbebd878ae7ba92da0a8d25d5b9d0b5c3ed3bcf
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-07 12:44:17 +0300

    fix(core): double space bar removes characters (#2040)

---
+++ projects/core/src/lib/plugins/double-space.plugin.ts
@@ -0,0 +1,74 @@
+import type {MaskitoPlugin} from '@maskito/core';
+
+import type {TypedInputEvent} from '../types';
+import {EventListener} from '../utils';
+
+const SPACE = ' ';
+
+/**
+ * 1. Android user (with G-board keyboard or similar) presses 1st space
+ * ```
+ * {type: "beforeinput", data: " ", inputType: "insertText"}
+ * ```
+ * 2. User presses 2nd space
+ * ```
+ * // Android tries to delete previously inserted space
+ * {type: "beforeinput", inputType: "deleteContentBackward"}
+ * {type: "beforeinput", data: ". ", inputType: "insertText"}
+ * ```
+ * ---------
+ * 1. MacOS user presses 1st space
+ * ```
+ * {type: "beforeinput", data: " ", inputType: "insertText"}
+ * ```
+ * 2. User presses 2nd space
+ * ```
+ * // MacOS automatically run `element.setSelectionRange(indexBeforeSpace, indexAfterSpace)` and then
+ * {type: "beforeinput", data: ". ", inputType: "insertText"}
+ * ```
+ * ---------
+ * @see https://github.com/taiga-family/maskito/issues/2023
+ */
+export function createDoubleSpacePlugin(): MaskitoPlugin {
+    let prevValue = '';
+    let prevCaretIndex = 0;
+    let prevEvent: TypedInputEvent | null = null;
+    let prevRejectedSpace = false;
+
+    return (element) => {
+        const eventListener = new EventListener(element);
+
+        eventListener.listen('beforeinput', (event) => {
+            const {value, selectionStart, selectionEnd} = element;
+            const rejectedSpace =
+                prevEvent?.inputType === 'insertText' &&
+                prevEvent?.data === SPACE &&
+                !value.slice(0, Number(selectionEnd)).endsWith(SPACE);
+
+            if (event.inputType === 'insertText' && event.data === `.${SPACE}`) {
+                if (
+                    prevEvent?.inputType === 'deleteContentBackward' &&
+                    prevRejectedSpace
+                ) {
+                    // Android
+                    element.value = prevValue;
+                    element.setSelectionRange?.(prevCaretIndex, prevCaretIndex);
+                } else if (rejectedSpace) {
+                    // Mac OS
+                    element.setSelectionRange?.(selectionStart, selectionStart);
+                }
+            }
+
+            prevRejectedSpace = rejectedSpace;
+            prevEvent = event;
+            prevValue = value;
+            prevCaretIndex = Number(
+                (rejectedSpace ? prevCaretIndex : selectionEnd) === value.length
+                    ? selectionEnd
+                    : selectionStart,
+            );
+        });
+
+        return () => eventListener.destroy();
+    };
+}
--- projects/core/src/lib/plugins/index.ts
+++ projects/core/src/lib/plugins/index.ts
@@ -1,3 +1,4 @@
 export * from './change-event-plugin';
+export * from './double-space.plugin';
 export * from './initial-calibration-plugin';
 export * from './strict-composition-plugin';
