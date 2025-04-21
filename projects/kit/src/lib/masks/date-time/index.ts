6aa34aa610cf140248bc7a691beb5aaba1f0e0cd
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-04-21 10:23:10 +0300

    fix(kit): missing export of `maskitoParseDateTime` & `maskitoStringifyDateTime` utilities (#2074)

--- projects/kit/src/lib/masks/date-time/index.ts
+++ projects/kit/src/lib/masks/date-time/index.ts
@@ -1,1 +1,6 @@
-export {maskitoDateTimeOptionsGenerator} from './date-time-mask';
+export * from './constants';
+export * from './date-time-mask';
+export * from './date-time-params';
+export * from './postprocessors';
+export * from './preprocessors';
+export * from './utils';
--- projects/kit/src/lib/masks/date-time/utils/index.ts
+++ projects/kit/src/lib/masks/date-time/utils/index.ts
@@ -1,2 +1,4 @@
 export * from './is-date-time-string-complete';
+export * from './parse-date-time';
 export * from './split-date-time-string';
+export * from './stringify-date-time';
