68e40e480cdda580502c3d320e8a8a2bd6a78fb0
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-06-04 17:49:35 +0300

    chore: use new approach

--- projects/react/src/lib/adaptControlledElement.ts
+++ projects/react/src/lib/adaptControlledElement.ts
@@ -4,41 +4,34 @@ import type {MaskitoElement} from '@maskito/core';
  * React adds `_valueTracker` property to every textfield elements for its internal logic with controlled inputs.
  * Also, React monkey-patches `value`-setter of the native textfield elements to update state inside its `_valueTracker`.
  * @see https://github.com/facebook/react/blob/ee76351917106c6146745432a52e9a54a41ee181/packages/react-dom-bindings/src/client/inputValueTracking.js#L12-L19
+ *
+ * React depends on `_valueTracker` to know if the value was changed to decide:
+ * - should it revert state for controlled input (if its state handler does not update value)
+ * - should it dispatch its synthetic (not native!) `change` event
+ *
+ * When Maskito patches textfield with a valid value (using setter of `value` property),
+ * it also updates `_valueTracker` state and React mistakenly decides that nothing has happened.
+ * React should update `_valueTracker` state by itself!
+ * ___
+ * @see https://github.com/facebook/react/blob/ee76351917106c6146745432a52e9a54a41ee181/packages/react-dom-bindings/src/client/inputValueTracking.js#L173-L177
  */
-type ReactControlledElement = MaskitoElement & {
-    _valueTracker: {
-        getValue(): string;
-        setValue(value: string): void;
-        stopTracking(): void;
-    };
-};
-
-const isReactControlledElement = (
-    element: MaskitoElement,
-): element is ReactControlledElement => '_valueTracker' in element;
-
 export function adaptReactControlledElement(element: MaskitoElement): MaskitoElement {
-    if (!isReactControlledElement(element)) {
+    const valueSetter = Object.getOwnPropertyDescriptor(
+        getPrototype(element),
+        'value',
+    )?.set;
+
+    if (!valueSetter) {
         return element;
     }
 
     const adapter = {
         set value(value: string) {
-            const lastValue = element._valueTracker.getValue();
-
-            element.value = value; // It will also update `_valueTracker` state
             /**
-             * React depends on `_valueTracker` to know if the value was changed to decide:
-             * - should it revert state for controlled input (if its state handler does not update value)
-             * - should it dispatch its synthetic (not native!) `change` event
-             * ___
-             * When Maskito patches textfield with a valid value (using setter of `value` property),
-             * it also updates `_valueTracker` state and React mistakenly decides that nothing has happened.
-             * React should update `_valueTracker` state by itself.
-             * ___
-             * @see https://github.com/facebook/react/blob/ee76351917106c6146745432a52e9a54a41ee181/packages/react-dom-bindings/src/client/inputValueTracking.js#L173-L177
+             * Mimics exactly what happens when a browser silently changes the value property.
+             * Bypass the React monkey-patching.
              */
-            element._valueTracker.setValue(lastValue);
+            valueSetter.call(element, value);
         },
     };
 
@@ -55,4 +48,17 @@ export function adaptReactControlledElement(element: MaskitoElement): MaskitoEle
             return Reflect.set(prop in adapter ? adapter : target, prop, val, receiver);
         },
     });
+}
+
+function getPrototype(
+    element: MaskitoElement,
+): HTMLInputElement | HTMLTextAreaElement | null | undefined {
+    switch (element.nodeName) {
+        case 'INPUT':
+            return globalThis.HTMLInputElement?.prototype;
+        case 'TEXTAREA':
+            return globalThis.HTMLTextAreaElement?.prototype;
+        default:
+            return null;
+    }
 }
