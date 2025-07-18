dde6a0e557fe0cd5268c2be2e3ebfae73aed27c6
l.golovina <l.golovina@tinkoff.ru>
2025-07-18 22:01:01 +0300

    feat(kit): fix limits in appendDate utils

--- projects/kit/src/lib/masks/date-range/constants.ts
+++ projects/kit/src/lib/masks/date-range/constants.ts
@@ -13,3 +13,22 @@ export const POSSIBLE_DATE_RANGE_SEPARATOR = [
     CHAR_MINUS,
     CHAR_JP_HYPHEN,
 ];
+
+export const MIN_DAY = 1;
+
+export const MONTHS_IN_YEAR = 12;
+
+export const MonthNumber = {
+    January: 0,
+    February: 1,
+    March: 2,
+    April: 3,
+    May: 4,
+    June: 5,
+    July: 6,
+    August: 7,
+    September: 8,
+    October: 9,
+    November: 10,
+    December: 11,
+} as const;
