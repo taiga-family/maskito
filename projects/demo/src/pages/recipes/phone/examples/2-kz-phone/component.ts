a5690cbf24d0f1d07d8189200ce1d593de44e8fa
Максим Иванов <omaxphp@yandex.ru>
2025-08-26 16:41:06 +0300

    chore(demo): update phone example (#2284)

--- projects/demo/src/pages/recipes/phone/examples/2-kz-phone/component.ts
+++ projects/demo/src/pages/recipes/phone/examples/2-kz-phone/component.ts
@@ -1,21 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from '@angular/forms';
 import {MaskitoDirective} from '@maskito/angular';
-import {TuiFlagPipe} from '@taiga-ui/core';
-import {TuiInputModule, TuiTextfieldControllerModule} from '@taiga-ui/legacy';
+import {TuiFlagPipe, TuiTextfield} from '@taiga-ui/core';
 
 import mask from './mask';
 
 @Component({
     standalone: true,
     selector: 'phone-doc-example-2',
-    imports: [
-        MaskitoDirective,
-        ReactiveFormsModule,
-        TuiFlagPipe,
-        TuiInputModule,
-        TuiTextfieldControllerModule,
-    ],
+    imports: [MaskitoDirective, ReactiveFormsModule, TuiFlagPipe, TuiTextfield],
     templateUrl: './template.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
