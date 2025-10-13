27c15956fb2a9c7d77af28daf879f3f3d8811ce6
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-10-13 09:57:03 +0300

    chore: use `inputs` (`@Directive` metadata) instead of `@Input()` decorator

--- projects/angular/src/lib/pattern.directive.ts
+++ projects/angular/src/lib/pattern.directive.ts
@@ -1,16 +1,16 @@
-import {Directive, inject, Input} from '@angular/core';
+import {Directive, inject} from '@angular/core';
 
 import {MaskitoDirective} from './maskito.directive';
 
 @Directive({
     standalone: true,
     selector: '[maskitoPattern]',
+    inputs: ['maskitoPattern'],
     hostDirectives: [MaskitoDirective],
 })
 export class MaskitoPattern {
     private readonly maskitoDirective = inject(MaskitoDirective, {self: true});
 
-    @Input()
     public set maskitoPattern(pattern: RegExp | string) {
         this.maskitoDirective.options.set({
             mask: typeof pattern === 'string' ? new RegExp(`^${pattern}$`) : pattern,
