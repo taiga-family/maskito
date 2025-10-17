d08d61df10ff03473a551c51e12e132dfb1489b6
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2025-10-17 14:55:18 +0300

    chore(demo): use `RouterTestingHarness` inside units for demo application (#2386)

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -1,49 +1,36 @@
 import {APP_BASE_HREF} from '@angular/common';
-import type {ComponentFixture} from '@angular/core/testing';
 import {TestBed} from '@angular/core/testing';
-import {Router} from '@angular/router';
+import {RouterTestingHarness} from '@angular/router/testing';
 import {DemoPath} from '@demo/constants';
 import {beforeEach, describe, expect, it} from '@jest/globals';
 
 import {App} from './app.component';
 import {APP_CONFIG} from './app.config';
 
-describe('Test dummy', () => {
-    let fixture: ComponentFixture<App>;
-
+describe('Ensure unit tests work for demo application', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [App],
             providers: [...APP_CONFIG.providers, {provide: APP_BASE_HREF, useValue: '/'}],
         });
-
-        fixture = TestBed.createComponent(App);
-        fixture.detectChanges();
     });
 
     it('appComponent compiles properly', () => {
+        const fixture = TestBed.createComponent(App);
+
+        fixture.detectChanges();
+
         expect(fixture.nativeElement.textContent).toContain('Getting started');
         expect(fixture.nativeElement.textContent).toContain('Core concepts');
     });
 
-    /**
-     * Angular 14 regression
-     * ___
-     * TODO: In Angular 15.2.0 use `RouterTestingHarness`:
-     * ```ts
-     * import {RouterTestingHarness} from '@angular/router/testing';
-     * // [...]
-     * const router = await RouterTestingHarness.create();
-     * const activatedComponent = await harness.navigateByUrl('/${DemoPath.WhatIsMaskito}', AppComponent);
-     * ```
-     *
-     * Learn more:
-     * - https://github.com/angular/angular/commit/dedac8d3f73ebf4f05b773454e2a22ab5fa4bf7c
-     * - https://blog.angular.io/write-better-tests-without-router-mocks-stubs-bf5fc95c1c57
-     */
-    it.skip('router works', async () => {
-        await TestBed.inject(Router).navigate([`/${DemoPath.WhatIsMaskito}`]);
+    it('router works', async () => {
+        const router = await RouterTestingHarness.create();
 
-        expect(fixture.nativeElement.textContent).toContain('Why Maskito?');
+        await router.navigateByUrl(DemoPath.CoreConceptsOverview);
+
+        expect(router.routeNativeElement?.textContent).toContain(
+            'The main entity of Maskito core library is Maskito class which accepts 2 arguments in constructor',
+        );
     });
 });
