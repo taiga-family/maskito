fee9aec4dccac6c731422aaa812a3a1d9305e690
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-05-19 12:27:36 +0300

    chore(demo): drop excessive `tui-icon[polymorpheusOutlet]`

--- projects/demo/src/pages/recipes/phone/examples/1-us-phone/component.ts
+++ projects/demo/src/pages/recipes/phone/examples/1-us-phone/component.ts
@@ -1,22 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MaskitoDirective} from '@maskito/angular';
-import {TuiIcon, TuiInput} from '@taiga-ui/core';
+import {TuiInput} from '@taiga-ui/core';
 import {TuiFlagPipe} from '@taiga-ui/kit';
-import {PolymorpheusOutlet} from '@taiga-ui/polymorpheus';
 
 import mask from './mask';
 
 @Component({
     selector: 'phone-doc-example-1',
-    imports: [
-        FormsModule,
-        MaskitoDirective,
-        PolymorpheusOutlet,
-        TuiFlagPipe,
-        TuiIcon,
-        TuiInput,
-    ],
+    imports: [FormsModule, MaskitoDirective, TuiFlagPipe, TuiInput],
     template: `
         <tui-textfield [style.max-width.rem]="20">
             <label tuiLabel>Enter a phone number</label>
@@ -26,17 +18,12 @@ import mask from './mask';
                 [maskito]="maskitoOptions"
                 [(ngModel)]="value"
             />
-            <ng-template #usFlag>
-                <img
-                    alt="Flag of the United States"
-                    width="28"
-                    [src]="'US' | tuiFlag"
-                    [style.border-radius.%]="50"
-                />
-            </ng-template>
-            <tui-icon
-                *polymorpheusOutlet="usFlag as src"
-                [icon]="src"
+
+            <img
+                alt="Flag of the United States"
+                width="28"
+                [src]="'US' | tuiFlag"
+                [style.border-radius.%]="50"
             />
         </tui-textfield>
     `,
--- projects/demo/src/pages/recipes/placeholder/examples/2-phone/component.ts
+++ projects/demo/src/pages/recipes/placeholder/examples/2-phone/component.ts
@@ -1,22 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MaskitoDirective} from '@maskito/angular';
-import {TuiIcon, TuiInput} from '@taiga-ui/core';
+import {TuiInput} from '@taiga-ui/core';
 import {TuiFlagPipe} from '@taiga-ui/kit';
-import {PolymorpheusOutlet} from '@taiga-ui/polymorpheus';
 
 import mask from './mask';
 
 @Component({
     selector: 'placeholder-doc-example-2',
-    imports: [
-        FormsModule,
-        MaskitoDirective,
-        PolymorpheusOutlet,
-        TuiFlagPipe,
-        TuiIcon,
-        TuiInput,
-    ],
+    imports: [FormsModule, MaskitoDirective, TuiFlagPipe, TuiInput],
     template: `
         <tui-textfield [style.max-width.rem]="20">
             <label tuiLabel>Enter US phone number</label>
@@ -26,17 +18,12 @@ import mask from './mask';
                 [maskito]="maskitoOptions"
                 [(ngModel)]="value"
             />
-            <ng-template #usFlag>
-                <img
-                    alt="Flag of the United States"
-                    width="28"
-                    [src]="'US' | tuiFlag"
-                    [style.border-radius.%]="50"
-                />
-            </ng-template>
-            <tui-icon
-                *polymorpheusOutlet="usFlag as src"
-                [icon]="src"
+
+            <img
+                alt="Flag of the United States"
+                width="28"
+                [src]="'US' | tuiFlag"
+                [style.border-radius.%]="50"
             />
         </tui-textfield>
     `,
