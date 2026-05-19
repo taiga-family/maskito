6068521dfd4f01a41d37195a4143077490432418
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-05-19 11:15:26 +0300

    chore(demo): improve logo style in documentation header

--- projects/demo/src/app/modules/logo/logo.component.ts
+++ projects/demo/src/app/modules/logo/logo.component.ts
@@ -1,11 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {TuiLink} from '@taiga-ui/core';
 import {PolymorpheusComponent} from '@taiga-ui/polymorpheus';
 
 @Component({
     selector: 'logo',
-    imports: [RouterLink, TuiLink],
+    imports: [RouterLink],
     templateUrl: './logo.template.html',
     styleUrl: './logo.style.less',
     changeDetection: ChangeDetectionStrategy.OnPush,
