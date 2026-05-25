324bcc143911756e7a5af6eed4b39538d1d7ca20
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-05-25 17:11:49 +0300

    chore: fix tests

--- projects/kit/src/lib/masks/date-time/date-time-mask.ts
+++ projects/kit/src/lib/masks/date-time/date-time-mask.ts
@@ -88,6 +88,7 @@ export function maskitoDateTimeOptionsGenerator({
                 dateTimeSeparator,
                 timeMode,
                 timeSegmentMaxValues,
+                timeSeparators: separators,
             }),
         ],
         postprocessors: [
