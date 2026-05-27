0d5234ade7dac05b298eb47193b0733a7ebbf865
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-05-27 18:50:00 +0300

    chore: change examples order

--- projects/demo/src/pages/kit/time/examples/2-am-pm/component.ts
+++ projects/demo/src/pages/kit/time/examples/2-am-pm/component.ts
@@ -2,16 +2,17 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MaskitoDirective} from '@maskito/angular';
 import {TuiInput} from '@taiga-ui/core';
+import {TuiFlagPipe} from '@taiga-ui/kit';
 
 import mask from './mask';
 
 @Component({
     selector: 'time-mask-doc-example-2',
-    imports: [FormsModule, MaskitoDirective, TuiInput],
+    imports: [FormsModule, MaskitoDirective, TuiFlagPipe, TuiInput],
     template: `
         <tui-textfield
             filler="HH:MM AA"
-            iconEnd="@tui.clock"
+            iconStart="@tui.clock"
             [style.max-width.rem]="20"
         >
             <label tuiLabel>Enter 12-hour time format</label>
@@ -19,6 +20,13 @@ import mask from './mask';
                 tuiInput
                 [maskito]="mask"
                 [(ngModel)]="value"
+            />
+
+            <img
+                width="28"
+                [attr.alt]="'USA flag'"
+                [src]="'US' | tuiFlag"
+                [style.border-radius.%]="50"
             />
         </tui-textfield>
     `,
