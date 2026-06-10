b2e1d3fa185ebfbce16b8d1e5bf620a98fa189f7
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2026-06-10 16:51:03 +0300

    chore(demo): broken example on documentation page `Phone` (#2769)

--- projects/demo/src/pages/phone/examples/4-lazy-metadata/component.ts
+++ projects/demo/src/pages/phone/examples/4-lazy-metadata/component.ts
@@ -1,7 +1,6 @@
-import {ChangeDetectionStrategy, Component, type OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, resource} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MaskitoDirective} from '@maskito/angular';
-import {MASKITO_DEFAULT_OPTIONS} from '@maskito/core';
 import {maskitoPhone} from '@maskito/phone';
 import {TuiInput} from '@taiga-ui/core';
 
@@ -18,20 +17,23 @@ import {TuiInput} from '@taiga-ui/core';
                 autocomplete="tel"
                 inputmode="tel"
                 tuiInput
-                [maskito]="mask"
+                [maskito]="mask.value() ?? null"
                 [(ngModel)]="value"
             />
         </tui-textfield>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PhoneMaskDocExample4 implements OnInit {
+export class PhoneMaskDocExample4 {
     protected value = '+7 920 123-4567';
-    protected mask = MASKITO_DEFAULT_OPTIONS;
 
-    public ngOnInit(): void {
-        import('libphonenumber-js/min/metadata').then(({default: metadata}) => () => {
-            this.mask = maskitoPhone({countryIsoCode: 'RU', metadata});
-        });
-    }
+    protected readonly mask = resource({
+        loader: async () =>
+            import('libphonenumber-js/min/metadata').then(({default: metadata}) =>
+                maskitoPhone({
+                    countryIsoCode: 'RU',
+                    metadata,
+                }),
+            ),
+    });
 }
