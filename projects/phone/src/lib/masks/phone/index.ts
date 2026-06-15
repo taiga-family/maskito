15b19283784e058ffadb53edead3dcdb60fb718f
Максим Иванов <omaxphp@yandex.ru>
2026-06-15 11:44:31 +0300

    Update index.ts

--- projects/phone/src/lib/masks/phone/index.ts
+++ projects/phone/src/lib/masks/phone/index.ts
@@ -1,8 +1,6 @@
 export {TEMPLATE_FILLER} from './constants';
 export type {MaskitoPhoneParams} from './phone-mask';
 export {maskitoPhone, maskitoPhoneOptionsGenerator} from './phone-mask';
-export {maskitoPhoneNonStrict} from './phone-mask-non-strict';
-export {maskitoPhoneStrict} from './phone-mask-strict';
 export {
     browserAutofillPreprocessorGenerator,
     cutInitCountryCodePreprocessor,
